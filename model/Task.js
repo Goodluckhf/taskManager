7b4d179b72320b7abe0ba182a908709344a6bf54
Konstantin <goodluckhf@yandex.ru>
2018-06-17 12:59:11 +0300

    feat(model/Task): add prop status

--- model/Task.js
+++ model/Task.js
@@ -1,12 +1,17 @@
 // @flow
 import mongoose from 'mongoose';
 import moment   from 'moment';
+import {
+	arrayToHash,
+	type ArrayToHashT,
+} from '../lib/helper';
 
 export type TaskPropsType = {
 	title     : string;
 	createdAt? : moment;
 };
 
+const statuses = ['pending', 'finished', 'skipped'];
 const schema = new mongoose.Schema({
 	title: {
 		type   : String,
@@ -16,11 +21,21 @@ const schema = new mongoose.Schema({
 		type   : Date,
 		default: moment.now,
 	},
+	
+	status: {
+		type: String,
+		enum: statuses,
+	},
 });
 
 export class TaskDocumentT /* :: extends Mongoose$Document */ {
 	title     : string;
 	createdAt : moment;
+	status    : string;
+	
+	static get status(): ArrayToHashT<string> {
+		return arrayToHash(statuses);
+	}
 	
 	static createInstance(opts : $Exact<TaskPropsType>): this {
 		return new this(opts);
