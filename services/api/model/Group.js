9e29ec7bfd30e527c06e92bc0ff24ce48ebe022a
Konstantin <goodluckhf@yandex.ru>
2018-08-11 21:38:56 +0300

    refactor(services/api/model/Public.js): Public -> Group

---
+++ services/api/model/Group.js
@@ -0,0 +1,76 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema({
+	publicId: {
+		type   : String,
+		default: null,
+	},
+	
+	name: {
+		type   : String,
+		default: null,
+	},
+	
+	domain: {
+		type   : String,
+		default: null,
+	},
+	
+	image: {
+		type   : String,
+		default: null,
+	},
+	
+	admin: {
+		type   : String,
+		default: null,
+	},
+});
+
+/**
+ * @property {Number} publicId
+ * @property {String} name
+ * @property {String} domain
+ * @property {String} image
+ * @property {String} admin
+ */
+class GroupDocument {
+	/**
+	 * @param {String} name
+	 * @param {String} photo_200
+	 * @param {Number} id
+	 * @param {String} screen_name
+	 */
+	/* eslint-disable camelcase */
+	// eslint-disable-next-line object-curly-newline
+	static createInstance({ name, photo_200, id, screen_name }) {
+		const group = new this();
+		
+		group.publicId = id;
+		group.name     = name;
+		group.image    = photo_200;
+		group.domain   = screen_name;
+		return group;
+	}
+	
+	/**
+	 * @param {Number} id
+	 * @param {Object} data
+	 * @return {GroupDocument}
+	 */
+	static async findOrCreateById(id, data) {
+		const group = await this.findOne({ publicId: id });
+		
+		if (group) {
+			return group;
+		}
+		
+		const newGroup = this.createInstance(data);
+		return newGroup.save();
+	}
+}
+/* eslint-enable camelcase */
+
+schema.loadClass(GroupDocument);
+
+export default schema;
