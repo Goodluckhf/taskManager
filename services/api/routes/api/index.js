9fd8049bcdcf5032c723cdffeb7d99650db04085
Konstantin <goodluckhf@yandex.ru>
2018-08-11 17:05:45 +0300

    refactor(services/api/routes/api/index.js): Заменил внедрением зависимостей в роут

--- services/api/routes/api/index.js
+++ services/api/routes/api/index.js
@@ -1,44 +1,22 @@
-// @flow
+import Router  from 'koa-router';
+import config  from 'config';
+import TaskApi from '../../api/TaskApi';
+import VkApi   from '../../../../lib/VkApi';
+import logger  from '../../../../lib/logger';
 
-import Router from 'koa-router';
-import {
-	create,
-	list,
-	finish,
-} from '../../api/task';
+const vkApi = new VkApi(config.get('vkApi.token'), {
+	timeout: config.get('vkApi.timeout'),
+});
 
-import { ValidationError }     from '../../api/errors';
+const taskApi = new TaskApi(config, vkApi, logger);
+
 
 const router = new Router({ prefix: '/api' });
 
-router.get('/tasks', async (ctx) => {
-	ctx.body = await list();
-});
-
 router.post('/task', async (ctx) => {
-	const { title } = ctx.request.body;
-	if (!title || title.length === 0) {
-		throw new ValidationError();
-	}
-	
 	ctx.body = {
 		success: true,
-		data   : await create({ title }),
-	};
-});
-
-router.post('/task/:id/finish', async (ctx) => {
-	const { id } : { id: string } = ctx.params;
-	
-	if (!id || id.length === 0) {
-		throw new ValidationError();
-	}
-	
-	const task = await finish(id);
-	
-	ctx.response.body = {
-		success: true,
-		data   : task,
+		data   : await taskApi.createLikes(ctx.request.body),
 	};
 });
 
