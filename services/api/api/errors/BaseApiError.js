745a67ae7bd7e435232b8867bcfffb29f1667f3f
Konstantin <goodluckhf@yandex.ru>
2018-08-12 12:44:02 +0300

    refactor(services/api/api/errors/BaseApiError.js): Добавил combine для примиксовывания ошибки

--- services/api/api/errors/BaseApiError.js
+++ services/api/api/errors/BaseApiError.js
@@ -5,19 +5,32 @@ export default class BaseApiError extends Error {
 	 */
 	constructor(message, status) {
 		super(message);
-		this.status  = status || 400;
-		this.success = false;
+		this.status      = status || 400;
+		this.success     = false;
+		this.extraParams = [];
 	}
 	
 	toObject() {
-		return {
+		const object = {
 			success: this.success,
 			message: this.message,
 			status : this.status,
 		};
+		
+		if (this.extraParams.length) {
+			object.extraParams = this.extraParams;
+		}
+		
+		return object;
 	}
 	
-	toJson() {
-		return JSON.stringify(this.toObject());
+	/**
+	 * @description Добавляет параметр к ошибке
+	 * @param {*} param
+	 * @return {BaseApiError}
+	 */
+	combine(param) {
+		this.extraParams.push(param);
+		return this;
 	}
 }
