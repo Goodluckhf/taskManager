68b4b20e69f72e5717fcf2eb04a3c73f44499c4b
Konstantin <goodluckhf@yandex.ru>
2018-08-17 02:41:57 +0300

    graceful shutdown

--- services/taskConsumer/index.js
+++ services/taskConsumer/index.js
@@ -12,8 +12,23 @@ const amqp = new Amqp(logger, {
 	retry: false,
 });
 
+let processing     = false;
+let needToShutDown = false;
+
+/**
+ * @param {Number} ms,
+ * @param {Number} code
+ */
+const forceExit = (ms = 500, code = 1) => {
+	setTimeout(() => {
+		process.exit(code);
+	}, ms);
+};
+
+
 class TaskResponse extends Response {
 	async process(method, data) {
+		processing = true;
 		this.logger.info({
 			message: 'receive method',
 			method,
@@ -21,6 +36,12 @@ class TaskResponse extends Response {
 		});
 		
 		await bluebird.delay(2000);
+		
+		if (needToShutDown) {
+			forceExit(500, 2);
+		}
+		
+		processing = false;
 		return {
 			testValue: Math.random(),
 		};
@@ -34,16 +55,6 @@ const response = new TaskResponse(logger, {
 
 const rpcServer = new RpcServer(amqp, logger, response);
 
-/**
- * @param {Number} ms,
- * @param {Number} code
- */
-const forceExit = (ms = 500, code = 1) => {
-	setTimeout(() => {
-		process.exit(code);
-	}, ms);
-};
-
 (async () => {
 	try {
 		await rpcServer.start();
@@ -56,6 +67,32 @@ const forceExit = (ms = 500, code = 1) => {
 		forceExit();
 	}
 })();
+
+process.on('SIGUSR1', () => {
+	logger.warn('usr1');
+});
+
+process.on('SIGHUP', () => {
+	logger.warn('SIGHUP');
+});
+
+process.on('SIGUSR2', () => {
+	logger.warn('usr2');
+});
+
+process.on('SIGINT', () => {
+	logger.warn('int');
+});
+
+process.on('SIGTERM', () => {
+	logger.warn({ message: 'Graceful shutdown' });
+	if (!processing) {
+		forceExit(500, 2);
+		return;
+	}
+	
+	needToShutDown = true;
+});
 
 process.on('uncaughtException', (error) => {
 	logger.error({ error });
