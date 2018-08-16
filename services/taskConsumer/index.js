817584f6679b5c3f98fa01753a6aa9c9c58ca31f
Konstantin <goodluckhf@yandex.ru>
2018-08-16 23:58:36 +0300

    feat(services/taskConsumer/index.js): Тест консумера

--- services/taskConsumer/index.js
+++ services/taskConsumer/index.js
@@ -1,56 +1,70 @@
-import config   from 'config';
-import bluebird from 'bluebird';
-import request  from 'request-promise';
-import amqp     from '../../lib/amqp';
-import logger   from '../../lib/logger';
+import config    from 'config';
+import bluebird  from 'bluebird';
 
-const processMessage = async (msg) => {
-	const data = JSON.parse(msg.content.toString());
-	logger.info({
-		message: 'Receive data',
-		data,
-	});
-	
-	await bluebird.delay(5000);
-	
-	const result = await request(`http://api:3000/api/task/${data.id}/finish`, {
-		method: 'POST',
-		json  : true,
-	});
-	logger.info({
-		message: `task "${data.id}" has successfully completed`,
-		result,
-	});
+import Amqp      from '../../lib/amqp/Amqp';
+import logger    from '../../lib/logger';
+import RpcServer from '../../lib/amqp/RpcServer';
+import Response  from '../../lib/amqp/Response';
+
+const rabbitConfig = config.get('rabbit');
+const amqp = new Amqp(logger, {
+	...rabbitConfig,
+	retry: false,
+});
+
+/**
+ * @param {Number} ms,
+ * @param {Number} code
+ */
+const forceExit = (ms = 500, code = 1) => {
+	setTimeout(() => {
+		process.exit(code);
+	}, ms);
 };
+
+class TaskResponse extends Response {
+	async process(method, data) {
+		this.logger.info({
+			message: 'receive method',
+			method,
+			data,
+		});
+		
+		await bluebird.delay(5000);
+		return {
+			testValue: Math.random(),
+		};
+	}
+}
 
 (async () => {
 	try {
-		const connection = await amqp;
-		const channel = await connection.createChannel();
-		await channel.assertQueue(config.get('taskQueue.name'));
-		channel.prefetch(config.get('taskQueue.prefetch'));
-		channel.consume(config.get('taskQueue.name'), async (msg) => {
-			if (msg === null) {
-				return;
-			}
-			
-			try {
-				await processMessage(msg);
-			} catch (error) {
-				channel.nack(msg);
-			}
-			
-			channel.ack(msg);
+		const connection = await amqp.connect();
+		
+		const response = new TaskResponse(logger, {
+			queue   : config.get('taskQueue.name'),
+			prefetch: config.get('taskQueue.prefetch'),
+		});
+		
+		const rpcServer = new RpcServer(connection, logger, response);
+		
+		await rpcServer.start();
+		logger.info({
+			message: 'rpc server started',
+			queue  : response.queue,
 		});
 	} catch (error) {
 		logger.error({ error });
+		forceExit();
 	}
 })();
 
 process.on('uncaughtException', (error) => {
 	logger.error({ error });
+	forceExit();
 });
 
 process.on('unhandledRejection', (error) => {
 	logger.error({ error });
+	forceExit();
 });
