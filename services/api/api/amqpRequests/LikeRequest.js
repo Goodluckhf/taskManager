da247dcc9d53026f5cf16d27d242a629b31dddc9
Konstantin <goodluckhf@yandex.ru>
2018-08-19 15:00:56 +0300

    fix(services/api/api/amqpRequests/LikeRequest.js): Поменял на spread для вызова базового конструктора

--- services/api/api/amqpRequests/LikeRequest.js
+++ services/api/api/amqpRequests/LikeRequest.js
@@ -1,12 +1,12 @@
 import Request from '../../../../lib/amqp/Request';
 
 class LikeRequest extends Request {
-	constructor(config, args) {
+	constructor(config, ...args) {
 		const queue   = config.get('likesTask.queue');
 		const timeout = config.get('likesTask.timeout');
 		const method  = 'setLikes';
 		super({
-			args,
+			...args,
 			queue,
 			method,
 			timeout,
