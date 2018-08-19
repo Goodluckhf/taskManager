8e4392280718c58de0abac0ec835cfdda9ee7cfa
Konstantin <goodluckhf@yandex.ru>
2018-08-19 13:43:23 +0300

    feat(services/api/api/amqpRequests/LikeRequest.js): Добавил класс request для задачи лайков

---
+++ services/api/api/amqpRequests/LikeRequest.js
@@ -0,0 +1,17 @@
+import Request from '../../../../lib/amqp/Request';
+
+class LikeRequest extends Request {
+	constructor(config, args) {
+		const queue   = config.get('likesTask.queue');
+		const timeout = config.get('likesTask.timeout');
+		const method  = 'setLikes';
+		super({
+			args,
+			queue,
+			method,
+			timeout,
+		});
+	}
+}
+
+export default LikeRequest;
