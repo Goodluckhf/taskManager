e4b0503ea033879372641c84d7631411dd6d6c1c
Konstantin <goodluckhf@yandex.ru>
2018-08-25 23:15:36 +0300

    refactor(services/api/routes/api/index.js): Вынес роут task в отдельный файл

--- services/api/routes/api/index.js
+++ services/api/routes/api/index.js
@@ -1,14 +1,12 @@
-import Router      from 'koa-router';
-import config      from 'config';
-import TaskApi     from '../../api/TaskApi';
-import VkApi       from '../../../../lib/VkApi';
-import logger      from '../../../../lib/logger';
-import RpcClient   from '../../../../lib/amqp/RpcClient';
-import Amqp        from '../../../../lib/amqp/Amqp';
+import config          from 'config';
+import Router          from 'koa-router';
 
-const vkApi = new VkApi(config.get('vkApi.token'), {
-	timeout: config.get('vkApi.timeout'),
-});
+import logger          from '../../../../lib/logger';
+import RpcClient       from '../../../../lib/amqp/RpcClient';
+import Amqp            from '../../../../lib/amqp/Amqp';
+
+import createTaskRoute    from './task';
+import createAccountRoute from './account';
 
 // rabbit, RPC client
 const rabbitConfig = config.get('rabbit');
@@ -18,56 +16,8 @@ rpcClient.start().catch((error) => {
 	logger.error({ error });
 });
 
-// Сам классс Api
-const taskApi = new TaskApi(rpcClient, config, vkApi, logger);
-
-
 const router = new Router({ prefix: '/api' });
 
-router.post('/task', async (ctx) => {
-	ctx.body = {
-		success: true,
-		data   : await taskApi.createLikes(ctx.request.body),
-	};
-});
-
-router.get('/task', async (ctx) => {
-	ctx.body = {
-		success: true,
-		data   : await taskApi.getActual(),
-	};
-});
-
-router.post('/task/stop/:id', async (ctx) => {
-	const { id } = ctx.params;
-	
-	ctx.body = {
-		success: true,
-		data   : await taskApi.stop(id),
-	};
-});
-
-router.get('/task/handleActive', async (ctx) => {
-	ctx.body = {
-		success: true,
-		data   : await taskApi.handleActiveTasks(),
-	};
-});
-
-router.put('/task/likes', async (ctx) => {
-	ctx.body = {
-		success: true,
-		data   : await taskApi.setLikes(ctx.request.body),
-	};
-});
-
-router.put('/task/:id', async (ctx) => {
-	const { id } = ctx.params;
-	
-	ctx.body = {
-		success: true,
-		data   : await taskApi.updateLikes(id, ctx.request.body),
-	};
-});
-
+createTaskRoute(router, rpcClient);
+createAccountRoute(router, rpcClient);
 export default router;
---
+++ services/api/routes/api/task.js
@@ -0,0 +1,58 @@
+import config  from 'config';
+import VkApi   from '../../../../lib/VkApi';
+import logger  from '../../../../lib/logger';
+import TaskApi from '../../api/TaskApi';
+
+export default (router, rpcClient) => {
+	const vkApi = new VkApi(config.get('vkApi.token'), {
+		timeout: config.get('vkApi.timeout'),
+	});
+	// Сам классс Api
+	const taskApi = new TaskApi(rpcClient, vkApi, config, logger);
+	
+	router.post('/task', async (ctx) => {
+		ctx.body = {
+			success: true,
+			data   : await taskApi.createLikes(ctx.request.body),
+		};
+	});
+	
+	router.get('/task', async (ctx) => {
+		ctx.body = {
+			success: true,
+			data   : await taskApi.getActual(),
+		};
+	});
+	
+	router.post('/task/stop/:id', async (ctx) => {
+		const { id } = ctx.params;
+		
+		ctx.body = {
+			success: true,
+			data   : await taskApi.stop(id),
+		};
+	});
+	
+	router.get('/task/handleActive', async (ctx) => {
+		ctx.body = {
+			success: true,
+			data   : await taskApi.handleActiveTasks(),
+		};
+	});
+	
+	router.put('/task/likes', async (ctx) => {
+		ctx.body = {
+			success: true,
+			data   : await taskApi.setLikes(ctx.request.body),
+		};
+	});
+	
+	router.put('/task/:id', async (ctx) => {
+		const { id } = ctx.params;
+		
+		ctx.body = {
+			success: true,
+			data   : await taskApi.updateLikes(id, ctx.request.body),
+		};
+	});
+};
