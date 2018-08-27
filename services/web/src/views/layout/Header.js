77aef96594a63f4f4af6a8af719191486ab031ba
Konstantin <goodluckhf@yandex.ru>
2018-08-28 00:25:04 +0300

    refactor(src/views/layout/Header.js): убрал лишнее

--- services/web/src/views/layout/Header.js
+++ services/web/src/views/layout/Header.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 import PropTypes from 'prop-types';
 
-import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
 import logo from '../../assets/img/brand/logo.svg';
 import sygnet from '../../assets/img/brand/sygnet.svg';
 
@@ -30,7 +30,7 @@ class Header extends Component {
 				/>
 				<AppSidebarToggler className="d-md-down-none" display="lg" />
 
-				<Nav className="d-md-down-none" navbar>
+				<Nav className="d-md-down-none ml-auto" navbar>
 					<NavItem className="px-3">
 						<NavLink href="/">Dashboard</NavLink>
 					</NavItem>
@@ -41,39 +41,6 @@ class Header extends Component {
 						<NavLink href="#">Settings</NavLink>
 					</NavItem>
 				</Nav>
-				<Nav className="ml-auto" navbar>
-					<NavItem className="d-md-down-none">
-						<NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>
-					</NavItem>
-					<NavItem className="d-md-down-none">
-						<NavLink href="#"><i className="icon-list"></i></NavLink>
-					</NavItem>
-					<NavItem className="d-md-down-none">
-						<NavLink href="#"><i className="icon-location-pin"></i></NavLink>
-					</NavItem>
-					<AppHeaderDropdown direction="down">
-						<DropdownToggle nav>
-							<img src={'assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-						</DropdownToggle>
-						<DropdownMenu right style={{ right: 'auto' }}>
-							<DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
-							<DropdownItem><i className="fa fa-bell-o"></i> Updates<Badge color="info">42</Badge></DropdownItem>
-							<DropdownItem><i className="fa fa-envelope-o"></i> Messages<Badge color="success">42</Badge></DropdownItem>
-							<DropdownItem><i className="fa fa-tasks"></i> Tasks<Badge color="danger">42</Badge></DropdownItem>
-							<DropdownItem><i className="fa fa-comments"></i> Comments<Badge color="warning">42</Badge></DropdownItem>
-							<DropdownItem header tag="div" className="text-center"><strong>Settings</strong></DropdownItem>
-							<DropdownItem><i className="fa fa-user"></i> Profile</DropdownItem>
-							<DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>
-							<DropdownItem><i className="fa fa-usd"></i> Payments<Badge color="secondary">42</Badge></DropdownItem>
-							<DropdownItem><i className="fa fa-file"></i> Projects<Badge color="primary">42</Badge></DropdownItem>
-							<DropdownItem divider />
-							<DropdownItem><i className="fa fa-shield"></i> Lock Account</DropdownItem>
-							<DropdownItem><i className="fa fa-lock"></i> Logout</DropdownItem>
-						</DropdownMenu>
-					</AppHeaderDropdown>
-				</Nav>
-				<AppAsideToggler className="d-md-down-none" />
-				{/*<AppAsideToggler className="d-lg-none" mobile />*/}
 			</React.Fragment>
 		);
 	}
