5beb524c3b9a42cf914929ec3531eddee0af8272
Konstantin <goodluckhf@yandex.ru>
2018-08-28 00:32:19 +0300

    Удалил еще остатки

--- services/web/src/views/layout/Footer.js
+++ services/web/src/views/layout/Footer.js
@@ -15,7 +15,7 @@ class Footer extends Component {
 		return (
 			<React.Fragment>
 				<span><a href="https://coreui.io">CoreUI</a> &copy; 2018 creativeLabs.</span>
-				<span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
+				<span className="ml-auto">Powered by <a href="Goodluckhf@yandex.ru">Хлопчики девелопчики</a></span>
 			</React.Fragment>
 		);
 	}
