8ca1cbcd7ecef391ae00f40300d226d25aab68f3
Konstantin <goodluckhf@yandex.ru>
2018-08-28 23:43:08 +0300

    feat(services/api/api/GroupApi.js): Добавил API класс GroupApi и метод add

---
+++ services/api/api/GroupApi.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+import BaseApi               from './BaseApi';
+import { GroupAlreadyExist } from './errors';
+
+/**
+ * @property {VkApi} vkApi
+ */
+class GroupApi extends BaseApi {
+	constructor(vkApi, ...args) {
+		super(...args);
+		this.vkApi = vkApi;
+	}
+	
+	/**
+	 * @description Добавляет новую группу
+	 * @param {Object} data
+	 * @param {String} data.link
+	 * @param {String} data.isTarget
+	 */
+	async add(data) {
+		const Group = mongoose.model('Group');
+		
+		this.validate({
+			properties: {
+				link    : { type: 'string' },
+				admin   : { type: 'string' },
+				isTarget: { type: 'string' },
+			},
+			required: ['link'],
+		}, data);
+		
+		const vkGroup = await this.vkApi.groupByHref(data.link);
+		const group   = await Group.findOne({ publicId: vkGroup.id });
+		if (group) {
+			throw new GroupAlreadyExist({ id: vkGroup.id, name: vkGroup.name });
+		}
+		
+		const newGroup = Group.createInstance(vkGroup);
+		
+		if (data.isTarget) {
+			newGroup.isTarget = data.isTarget;
+		}
+		
+		return newGroup.save();
+	}
+}
+
+export default GroupApi;
