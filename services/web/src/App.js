fb786f1d32a0e1a3dabc4aa78646d307c510d736
Konstantin <goodluckhf@yandex.ru>
2018-08-29 23:59:16 +0300

    feat(src/App.js): Обернул в redux

--- services/web/src/App.js
+++ services/web/src/App.js
@@ -1,5 +1,7 @@
-import React, { Component }                from 'react';
-import { BrowserRouter, Route, Switch }       from 'react-router-dom';
+import React, { Component }            	from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider }                     from 'react-redux';
+
 // CoreUI Icons Set
 import '@coreui/icons/css/coreui-icons.min.css';
 // Import Flag Icons Set
@@ -11,19 +13,25 @@ import 'simple-line-icons/css/simple-line-icons.css';
 // Import Main styles for this application
 import './scss/style.css';
 
-import { Page404, HomePage } from './views/pages';
+import { Page404, HomePage }            from './views/pages';
+import { configureStore, initialState } from './store';
+
+const store = configureStore(initialState);
+
 
 class App extends Component {
 	//eslint-disable-next-line class-methods-use-this
 	render() {
 		return (
-			<BrowserRouter>
-				<Switch>
-					<Route exact path="/404" name="Page 404" component={Page404} />
-					<Route path="/" exact name="Home" component={HomePage} />
-					<Route component={Page404} />
-				</Switch>
-			</BrowserRouter>
+			<Provider store={store}>
+				<BrowserRouter>
+					<Switch>
+						<Route exact path="/404" name="Page 404" component={Page404} />
+						<Route path="/" exact name="Home" component={HomePage} />
+						<Route component={Page404} />
+					</Switch>
+				</BrowserRouter>
+			</Provider>
 		);
 	}
 }
