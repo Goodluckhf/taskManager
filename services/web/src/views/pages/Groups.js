c4ef542cf1bd58136c17ca326c4492057d12e8df
Konstantin <goodluckhf@yandex.ru>
2018-08-30 23:22:07 +0300

    Пробую создать форму

--- services/web/src/views/pages/Groups.js
+++ services/web/src/views/pages/Groups.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import Layout      from '../layout/Layout';
+import {
+	Card, CardHeader, CardBody,
+	Container, Row, Col,
+} from 'reactstrap';
+
+import Layout from '../layout/Layout';
 
 class Groups extends Component {
 	//eslint-disable-next-line class-methods-use-this
 	render() {
 		return (
 			<Layout>
-				<h1>Паблики</h1>
+				<Container fluid={true}>
+					<Row>
+						<Col xs={12}>
+							<Card>
+								<CardHeader><b>Паблики</b></CardHeader>
+								<CardBody>Тут текст</CardBody>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
 			</Layout>
 		);
 	}
