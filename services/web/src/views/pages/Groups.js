c9f792bd2a043e2a158b8fd2d25d26efdcf25fdd
Konstantin <goodluckhf@yandex.ru>
2018-08-31 01:24:30 +0300

    feat(src/views/pages/Groups.js): Сложил все на страницу и подключил к redux

--- services/web/src/views/pages/Groups.js
+++ services/web/src/views/pages/Groups.js
@@ -1,50 +1,31 @@
 import React, { Component } from 'react';
-import {
-	Card, CardHeader, CardBody,
-	Container, Form, FormGroup,
-	Label, Input, Button, CardFooter,
-} from 'reactstrap';
+import { Container } from 'reactstrap';
+import { connect }   from 'react-redux';
 
-import Layout from '../layout/Layout';
+import Form         from '../components/group/Form';
+import Layout       from '../layout/Layout';
+import { addGroup } from '../../actions/groups';
+import List         from '../components/group/List';
 
 class Groups extends Component {
-	constructor(props) {
-		super(props);
-		
-		this.state = { link: '' };
-	}
-	
-	handleInput = (e) => {
-		this.setState({ link: e.target.value });
-	};
-	
-	handleSubmit = () => {
-		console.log(this.state.link);
-	};
-	
-	//eslint-disable-next-line class-methods-use-this
 	render() {
 		return (
 			<Layout>
 				<Container fluid={true}>
-					<Card>
-						<CardHeader><b>Добавить паблик</b></CardHeader>
-						<CardBody>
-							<Form>
-								<FormGroup>
-									<Label>Ссылка на паблик</Label>
-									<Input onChange={this.handleInput} type='text' placeholder='https://vk.com/nice.advice'/>
-								</FormGroup>
-							</Form>
-						</CardBody>
-						<CardFooter>
-							<Button onClick={this.handleSubmit}>Добавить</Button>
-						</CardFooter>
-					</Card>
+					<Form {...this.props}/>
+					<List {...this.props}/>
 				</Container>
 			</Layout>
 		);
 	}
 }
 
-export default Groups;
+const mapDispatchToProps = dispatch => ({
+	addGroup: link => dispatch(addGroup(link)),
+});
+
+const mapStateToProps = state => ({
+	groups: state.get('groups'),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Groups);
