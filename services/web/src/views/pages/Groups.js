9796c7a25b7e58a79dd95de29039786da5e81729
Konstantin <goodluckhf@yandex.ru>
2018-08-30 23:51:01 +0300

    feat(src/views/pages/Groups.js): Начало разработки формы

--- services/web/src/views/pages/Groups.js
+++ services/web/src/views/pages/Groups.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
 import {
 	Card, CardHeader, CardBody,
-	Container,
+	Container, Form, FormGroup,
+	Label, Input, Button, CardFooter,
 } from 'reactstrap';
 
 import Layout from '../layout/Layout';
 
 class Groups extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = { link: '' };
+	}
+	
+	handleInput = (e) => {
+		this.setState({ link: e.target.value });
+	};
+	
+	handleSubmit = () => {
+		console.log(this.state.link);
+	};
+	
 	//eslint-disable-next-line class-methods-use-this
 	render() {
 		return (
 			<Layout>
 				<Container fluid={true}>
 					<Card>
-						<CardHeader><b>Паблики</b></CardHeader>
-						<CardBody>Тут текст</CardBody>
+						<CardHeader><b>Добавить паблик</b></CardHeader>
+						<CardBody>
+							<Form>
+								<FormGroup>
+									<Label>Ссылка на паблик</Label>
+									<Input onChange={this.handleInput} type='text' placeholder='https://vk.com/nice.advice'/>
+								</FormGroup>
+							</Form>
+						</CardBody>
+						<CardFooter>
+							<Button onClick={this.handleSubmit}>Добавить</Button>
+						</CardFooter>
 					</Card>
 				</Container>
 			</Layout>
