59a50e2e7781ced29897fe5d4f661cf36571e531
Konstantin <goodluckhf@yandex.ru>
2018-09-01 23:20:31 +0300

    feat(src/actions/groups.js): Добавил actions для группы на создание

--- services/web/src/actions/groups.js
+++ services/web/src/actions/groups.js
@@ -1,6 +1,20 @@
-export const ADD_GROUP = 'ADD_GROUP';
+// Создание группы - приходит от саги
+export const CREATE = 'GROUPS__CREATE_GROUP';
+export const create = group => ({
+	type   : CREATE,
+	payload: { group },
+});
 
-export const addGroup = link => ({
-	type   : ADD_GROUP,
+export const CREATE_FAILED = 'GROUPS__CREATE_GROUP_FAILED';
+export const createFailed  = error => ({
+	type   : CREATE_FAILED,
+	payload: { error },
+});
+
+
+// Для редьюсера
+export const REQUEST_CREATE = 'GROUPS__REQUEST_CREATE_GROUP';
+export const requestCreate  = link => ({
+	type   : REQUEST_CREATE,
 	payload: { link },
 });
