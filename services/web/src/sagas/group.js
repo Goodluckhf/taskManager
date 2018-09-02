71824e118788f87b1c1e5fa48e3ce51bdf1cc43a
Konstantin <goodluckhf@yandex.ru>
2018-09-02 17:10:02 +0300

    feat(services/web/src/sagas/group.js): Добавил сагу на изиенеия isTarget

--- services/web/src/sagas/group.js
+++ services/web/src/sagas/group.js
@@ -4,13 +4,13 @@ import axios from 'axios';
 
 import {
 	REQUEST_CREATE, REQUEST_LIST,
-	createFailed, create, list,
+	createFailed, create, list, CHANGE_IS_TARGET,
 } from '../actions/groups';
 
 export default function* () {
-	yield takeEvery(REQUEST_CREATE, function* ({ payload: { link } }) {
+	yield takeEvery(REQUEST_CREATE, function* ({ payload: data }) {
 		try {
-			const { data: result } = yield call(axios.post, '/api/group', { link });
+			const { data: result } = yield call(axios.post, '/api/group', data);
 			if (!result.success) {
 				yield put(createFailed(result.data));
 				return;
@@ -31,5 +31,10 @@ export default function* () {
 		const { data: result } = yield call(axios.get, '/api/groups');
 		//@TODO: Добавить обработку ошибки
 		yield put(list(result.data));
+	});
+	
+	yield takeEvery(CHANGE_IS_TARGET, function* ({ payload: { id, isTarget } }) {
+		// @TODO: Сделать обработку ошибки
+		yield call(axios.put, `/api/group/${id}/target`, { isTarget });
 	});
 }
