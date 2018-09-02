c153fd069d08efc37f4a792a48cc8bfc7339b362
Konstantin <goodluckhf@yandex.ru>
2018-09-02 13:34:44 +0300

    fix(src/sagas/group.js): обновил ошибку

--- services/web/src/sagas/group.js
+++ services/web/src/sagas/group.js
@@ -1,5 +1,4 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
 
 import axios from 'axios';
 
@@ -8,7 +7,6 @@ import { REQUEST_CREATE, createFailed, create } from '../actions/groups';
 export default function* () {
 	yield takeEvery(REQUEST_CREATE, function* ({ payload: { link } }) {
 		try {
-			yield call(delay, 4000);
 			const { data: result } = yield call(axios.post, '/api/group', { link });
 			if (!result.success) {
 				yield put(createFailed(result.data));
@@ -17,7 +15,11 @@ export default function* () {
 			
 			yield put(create(result.data));
 		} catch (error) {
-			console.log(error);
+			if (error.response && error.response.data) {
+				yield put(createFailed(error.response.data));
+				return;
+			}
+			
 			yield put(createFailed(error));
 		}
 	});
