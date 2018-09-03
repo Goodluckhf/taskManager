ba01a87a8423b443e58b38e3f3502ffbd3ced0fc
Konstantin <goodluckhf@yandex.ru>
2018-09-04 00:28:22 +0300

    feat(services/web/src/reducers/autolikes.js): Добавил тестовый редьюсер

---
+++ services/web/src/reducers/autolikes.js
@@ -0,0 +1,6 @@
+import { autoLikesPage } from '../store/initialState';
+
+//eslint-disable-next-line
+export default (state = autoLikesPage, { type, payload }) => {
+	return state;
+};
--- services/web/src/reducers/index.js
+++ services/web/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routes }    from '../store/initialState';
 import groupPage     from './groups';
+import autoLikesPage from './autolikes';
 import fatalError    from './fatalError';
 
 export default combineReducers({
 	routes: (state = routes) => state,
 	groupPage,
+	autoLikesPage,
 	fatalError,
 });
