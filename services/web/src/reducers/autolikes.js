598b1bf10c87ef1f1cdeb21bcc4a750b9ff9ea72
Konstantin <goodluckhf@yandex.ru>
2018-09-04 22:21:38 +0300

    feat(services/web/src/sagas/autolikes.js): Добавил reducer на создание задачи

--- services/web/src/reducers/autolikes.js
+++ services/web/src/reducers/autolikes.js
@@ -1,6 +1,24 @@
-import { autoLikesPage } from '../store/initialState';
+import { autoLikesPage }                         from '../store/initialState';
+import { REQUEST_CREATE, CREATE_FAILED, CREATE } from '../actions/autolikes';
 
 //eslint-disable-next-line
 export default (state = autoLikesPage, { type, payload }) => {
+	if (type === REQUEST_CREATE) {
+		return state.updateIn(
+			['form'],
+			form => form.set('loading', true),
+		);
+	}
+	
+	if (type === CREATE_FAILED || type === CREATE) {
+		console.log(payload);
+		return state.updateIn(
+			['form'],
+			form => form
+				.set('loading', false)
+				.set('error', payload.error),
+		);
+	}
+	
 	return state;
 };
