5c64b705d83d11871f00b76b3bab7b3ec13a4a27
Konstantin <goodluckhf@yandex.ru>
2018-09-05 22:26:34 +0300

    feat(services/web/src/sagas/autolikes.js): сага для списка

--- services/web/src/sagas/autolikes.js
+++ services/web/src/sagas/autolikes.js
@@ -1,20 +1,42 @@
 import axios from 'axios';
 
-import { takeEvery, put, call }                 from 'redux-saga/effects';
-import { REQUEST_CREATE, create, createFailed } from '../actions/autolikes';
-import { fatalError }                           from '../actions/fatalError';
+import { takeEvery, put, call } from 'redux-saga/effects';
+import {
+	REQUEST_CREATE, REQUEST_LIST,
+	create, createFailed, list,
+} from '../actions/autolikes';
+import { fatalError }           from '../actions/fatalError';
 
 export default function* () {
 	yield takeEvery(REQUEST_CREATE, function* ({ payload: data }) {
 		try {
 			const { data: result } = yield call(axios.post, '/api/task', data);
-			yield put(create(result));
+			if (!result.success) {
+				yield put(createFailed(result.data));
+				return;
+			}
+			
+			yield put(create(result.data));
 		} catch (error) {
 			if (error.response && error.response.data) {
 				yield put(createFailed(error.response.data));
 				return;
 			}
 			
+			yield put(fatalError(error));
+		}
+	});
+	
+	yield takeEvery(REQUEST_LIST, function* () {
+		try {
+			const { data: result } = yield call(axios.get, '/api/tasks');
+			if (!result.success) {
+				yield put(fatalError(result.data));
+				return;
+			}
+			
+			yield put(list(result.data));
+		} catch (error) {
 			yield put(fatalError(error));
 		}
 	});
