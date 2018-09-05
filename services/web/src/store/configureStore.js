38fd0d66f90bc67fb17564303e98c648cc45040e
Konstantin <goodluckhf@yandex.ru>
2018-09-05 13:37:07 +0300

    feat(src/store/configureStore.js): Добавил в создание стора мидлвары для redux router

--- services/web/src/store/configureStore.js
+++ services/web/src/store/configureStore.js
@@ -1,15 +1,21 @@
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware             from 'redux-saga';
 //eslint-disable-next-line import/no-extraneous-dependencies
-import { composeWithDevTools }          from 'redux-devtools-extension';
+import { composeWithDevTools }             from 'redux-devtools-extension';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
 
-import rootReducer from '../reducers';
-import rootSage    from '../sagas';
+import initialState from './initialState';
+import rootReducer  from '../reducers';
+import rootSage     from '../sagas';
 
-export default (initialState = {}) => {
-	const sagaMiddleware    = createSagaMiddleware();
-	const composedEnhancers = composeWithDevTools(applyMiddleware(sagaMiddleware));
-	const store = createStore(rootReducer, initialState, composedEnhancers);
+export default (history) => {
+	const sagaMiddleware = createSagaMiddleware();
+	const middlewares    = applyMiddleware(
+		sagaMiddleware,
+		routerMiddleware(history),
+	);
+	const composedEnhancers = composeWithDevTools(middlewares);
+	const store = createStore(connectRouter(history)(rootReducer), initialState, composedEnhancers);
 	sagaMiddleware.run(rootSage);
 	return store;
 };
