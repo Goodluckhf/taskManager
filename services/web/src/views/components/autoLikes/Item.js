87feda13642e2e9d05d60204089a0df7f2b17277
Konstantin <goodluckhf@yandex.ru>
2018-09-06 11:21:06 +0300

    feat(services/web/src/views/components/autoLikes/Item.js): Добавил вывод статуса

--- services/web/src/views/components/autoLikes/Item.js
+++ services/web/src/views/components/autoLikes/Item.js
@@ -4,11 +4,19 @@ import { Row, Col } from 'reactstrap';
 import * as moment from 'moment';
 import 'moment/locale/ru';
 
+const statusCodeToString = {
+	0: <span className='text-primary'>Ожидает</span>,
+	1: <span className='text-warning'>Ожидает</span>,
+	2: <span className='text-success'>Выполнена</span>,
+	3: <span className='text-muted'>Отменена</span>,
+};
+
 class Item extends PureComponent {
 	static propTypes = {
 		children  : propTypes.node,
 		createdAt : propTypes.instanceOf(Date),
 		likesCount: propTypes.number,
+		status    : propTypes.number,
 	};
 	
 	render() {
@@ -19,6 +27,7 @@ class Item extends PureComponent {
 					<h4>Описание</h4>
 					<div><span className='h6'>Кол-во лайков:</span> {this.props.likesCount}</div>
 					<div><span className='h6'>Создана:</span> {moment(this.props.createdAt).format('MMMM Do YYYY, h:mm:ss')}</div>
+					<div><span className='h6'>Статус:</span> {statusCodeToString[this.props.status]}</div>
 				</Col>
 				<Col>
 					<h4>Действия</h4>
