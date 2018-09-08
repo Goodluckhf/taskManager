713382ff5853ba9ed29c367165c129102f024826
Konstantin <goodluckhf@yandex.ru>
2018-09-08 19:04:53 +0300

    refactor(services/web/src/views/pages/AutoLikes.js): Добавил селектор

--- services/web/src/views/pages/AutoLikes.js
+++ services/web/src/views/pages/AutoLikes.js
@@ -6,7 +6,7 @@ import Immutable                    from 'immutable';
 import Layout                       from '../layout/Layout';
 import Form                         from '../components/autoLikes/Form';
 import List                         from '../components/autoLikes/List';
-import { requestCreate, requestFilterChange, stopRequest } from '../../actions/autolikes';
+import { createRequest, filterChangeRequest, stopRequest } from '../../actions/autolikes';
 import { loaderSelector } from '../../lib/loader';
 
 class AutoLikes extends PureComponent {
@@ -16,6 +16,7 @@ class AutoLikes extends PureComponent {
 		createAutoLike: propTypes.func,
 		filterChange  : propTypes.func,
 		stop          : propTypes.func,
+		loading       : propTypes.bool,
 	};
 	
 	
@@ -33,6 +34,7 @@ class AutoLikes extends PureComponent {
 						stop={this.props.stop}
 						filterChange={this.props.filterChange}
 						items={this.props.list}
+						loading={this.props.loading}
 					/>
 				</Container>
 			</Layout>
@@ -41,14 +43,14 @@ class AutoLikes extends PureComponent {
 }
 
 const mapDispatchToProps = dispatch => ({
-	createAutoLike: data => dispatch(requestCreate(data)),
-	filterChange  : filter => dispatch(requestFilterChange(filter)),
+	createAutoLike: data => dispatch(createRequest(data)),
+	filterChange  : filter => dispatch(filterChangeRequest(filter)),
 	stop          : id => dispatch(stopRequest(id)),
 });
 
 const mapStateToProps = state => ({
-	form: state.autoLikesPage.get('form'),
-	list: loaderSelector({ AUTO_LIKES__STOP: 'stop_loading', AUTO_LIKES__START: '' }, 'autoLikesPage', state, ['list']),
+	form: loaderSelector({ AUTO_LIKES__CREATE: 'loading' }, 'autoLikesPage', state, ['form']),
+	list: loaderSelector({ AUTO_LIKES__STOP: 'stop_loading', AUTO_LIKES__START: '' }, 'autoLikesPage', state, ['list', 'items']),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AutoLikes);
