b0ceaa11bf34c0786ddf35274b4d91b37d1e6f6a
Konstantin <goodluckhf@yandex.ru>
2018-09-10 21:53:45 +0300

    feat(services/web/src/views/components/ui/ApiError.js): Добавил title для отображения ошибки

--- services/web/src/views/components/ui/ApiError.js
+++ services/web/src/views/components/ui/ApiError.js
@@ -5,13 +5,15 @@ import { Alert } from 'reactstrap';
 class ApiError extends PureComponent {
 	static propTypes = {
 		error: propTypes.object,
+		title: propTypes.string,
 	};
 	
 	render() {
 		const { message } = this.props.error;
+		const title = this.props.title ? `${this.props.title}: ${message}` : message;
 		return (
 			<Alert color="danger" {...this.props}>
-				<h4 className="alert-heading">{message}!</h4>
+				<h4 className="alert-heading">{title}!</h4>
 				<pre>{JSON.stringify(this.props.error, null, 2)}</pre>
 			</Alert>
 		);
