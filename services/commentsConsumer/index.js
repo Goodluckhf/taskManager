146137992711714d5ac3934a3ef7bca34df5a980
Konstantin <goodluckhf@yandex.ru>
2018-09-12 00:11:57 +0300

    feat(services/commentsConsumer/Dockerfile): добавил как отдельный сервис

---
+++ services/commentsConsumer/index.js
@@ -0,0 +1,61 @@
+import config    from 'config';
+
+import Amqp             from '../../lib/amqp/Amqp';
+import logger           from '../../lib/logger';
+import RpcServer        from '../../lib/amqp/RpcServer';
+import CommentsResponse from './CommentsResponse';
+
+const rabbitConfig = config.get('rabbit');
+const amqp = new Amqp(logger, {
+	host : process.env.NODE_ENV === 'development' ? 'localhost' : rabbitConfig.host,
+	port : rabbitConfig.port,
+	retry: false,
+});
+
+/**
+ * @param {Number} ms,
+ * @param {Number} code
+ */
+const forceExit = (ms = 500, code = 1) => {
+	setTimeout(() => {
+		process.exit(code);
+	}, ms);
+};
+
+(async () => {
+	try {
+		const response = new CommentsResponse({
+			logger,
+			login   : config.get('z1y1x1.login'),
+			password: config.get('z1y1x1.password'),
+			queue   : config.get('commentsTask.queue'),
+			prefetch: config.get('commentsTask.prefetch'),
+		});
+		
+		const rpcServer = new RpcServer(amqp, logger, response);
+		await rpcServer.start();
+		logger.info({
+			message: 'rpc server started',
+			queue  : response.queue,
+		});
+	} catch (error) {
+		logger.error({ error });
+		forceExit();
+	}
+})();
+
+process.on('SIGTERM', () => {
+	//TODO: Сделать graceful shutdown
+	logger.warn({ message: 'Graceful shutdown' });
+	forceExit(500, 2);
+});
+
+process.on('uncaughtException', (error) => {
+	logger.error({ error });
+	forceExit();
+});
+
+process.on('unhandledRejection', (error) => {
+	logger.error({ error });
+	forceExit();
+});
