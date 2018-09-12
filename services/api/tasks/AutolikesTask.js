7d3353b3811ea965c04eeabd15f50e9eea5ca489
Konstantin <goodluckhf@yandex.ru>
2018-09-12 14:07:55 +0300

    refactor(services/api/tasks/AutolikesTask.js): Вызов таска для лайков из отдельного класса

--- services/api/tasks/AutolikesTask.js
+++ services/api/tasks/AutolikesTask.js
@@ -2,59 +2,58 @@ import moment       from 'moment/moment';
 import { JSDOM }    from 'jsdom';
 import mongoose     from 'mongoose';
 
-import BaseTask     from './BaseTask';
-import BaseApiError from '../api/errors/BaseApiError';
-import LikeRequest  from '../api/amqpRequests/LikeRequest';
+import BaseTask  from './BaseTask';
+import LikesTask from './LikesTask';
 
 /**
  * @property {VkApi} vkApi
  */
 class AutoLikesTask extends BaseTask {
-	constructor(vkApi, ...args) {
-		super(...args);
+	constructor({ vkApi, ...args }) {
+		super(args);
 		this.vkApi = vkApi;
 	}
 	
 	async handle() {
-		const Task      = mongoose.model('Task');
-		const Group     = mongoose.model('Group');
-		const LikesTask = mongoose.model('LikesTask');
+		const Task           = mongoose.model('Task');
+		const Group          = mongoose.model('Group');
+		const LikesTaskModel = mongoose.model('LikesTask');
 		
 		// Проверяем, что прошло 70 минут, чтобы не лайкать уже лайкнутый пост
-		this.task.status = Task.status.pending;
-		await this.task.save();
+		this.taskDocument.status = Task.status.pending;
+		await this.taskDocument.save();
 		const likesInterval = parseInt(this.config.get('autoLikesTask.likesInterval'), 10);
-		if (this.task.lastLikedAt && moment().diff(moment(this.task.lastLikedAt), 'minutes') < likesInterval) {
-			this.task.status = Task.status.waiting;
-			await this.task.save();
+		if (this.taskDocument.lastLikedAt && moment().diff(moment(this.taskDocument.lastLikedAt), 'minutes') < likesInterval) {
+			this.taskDocument.status = Task.status.waiting;
+			await this.taskDocument.save();
 			return;
 		}
 		
-		if (!this.task.group) {
+		if (!this.taskDocument.group) {
 			this.logger.warn({
 				message: 'Like task has no group',
-				taskId : this.task._id,
+				taskId : this.taskDocument._id,
 			});
-			this.task.status = Task.status.waiting;
-			await this.task.save();
+			this.taskDocument.status = Task.status.waiting;
+			await this.taskDocument.save();
 			return;
 		}
 		
 		const targetPublics = await Group.find({ isTarget: true }).lean().exec();
 		
 		if (!targetPublics.length) {
-			this.task.status = Task.status.waiting;
-			await this.task.save();
+			this.taskDocument.status = Task.status.waiting;
+			await this.taskDocument.save();
 			return;
 		}
 		
-		const link  = Group.getLinkByPublicId(this.task.group.publicId);
+		const link  = Group.getLinkByPublicId(this.taskDocument.group.publicId);
 		const jsDom = await JSDOM.fromURL(link);
 		
 		const $mentionLink = jsDom.window.document.querySelectorAll('#page_wall_posts .post .wall_post_text a.mem_link')[0];
 		if (!$mentionLink) {
-			this.task.status = Task.status.waiting;
-			await this.task.save();
+			this.taskDocument.status = Task.status.waiting;
+			await this.taskDocument.save();
 			return;
 		}
 		
@@ -70,49 +69,27 @@ class AutoLikesTask extends BaseTask {
 		});
 		
 		if (!hasTargetGroupInTask) {
-			this.task.status = Task.status.waiting;
-			await this.task.save();
+			this.taskDocument.status = Task.status.waiting;
+			await this.taskDocument.save();
 			return;
 		}
 		
-		const likesTask = LikesTask.createInstance({
+		const likesTaskDocument = LikesTaskModel.createInstance({
 			postLink,
-			likesCount: this.task.likesCount,
+			likesCount: this.taskDocument.likesCount,
 			status    : Task.status.pending,
-			parentTask: this.task,
+			parentTask: this.taskDocument,
 		});
-		this.task.subTasks.push(likesTask);
-		await likesTask.save();
 		
-		const request = new LikeRequest(this.config, {
-			postLink,
-			likesCount: this.task.likesCount,
+		const likesTask = new LikesTask({
+			logger      : this.logger,
+			parentTask  : this.taskDocument,
+			taskDocument: likesTaskDocument,
+			rpcClient   : this.rpcClient,
+			config      : this.config,
 		});
-		this.logger.info({ request });
 		
-		(async () => {
-			try {
-				const result = await this.rpcClient.call(request);
-				if (result.error) {
-					throw result.error;
-				}
-			} catch (error) {
-				const wrapedError = new BaseApiError(error.message, 500).combine(error);
-				this.logger.error({ error });
-				likesTask._error  = wrapedError.toObject();
-				await this.vkApi.apiRequest('messages.send', {
-					chat_id: this.config.get('vkAlert.chat_id'),
-					message: JSON.stringify(wrapedError.toObject(), null, 2),
-				});
-			}
-			
-			likesTask.status =  Task.status.finished;
-			await likesTask.save();
-			//eslint-disable-next-line no-param-reassign
-			this.task.lastLikedAt = new Date();
-			this.task.status = Task.status.waiting;
-			await this.task.save();
-		})();
+		await likesTask.handle();
 	}
 }
 
