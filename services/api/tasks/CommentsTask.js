c9788c00dcfbea36174ab803563e99e0c49e6673
Konstantin <goodluckhf@yandex.ru>
2018-09-13 00:08:42 +0300

    feat(services/api/tasks/CommentsTask.js): Почистил таску и сделал ее изолированной

--- services/api/tasks/CommentsTask.js
+++ services/api/tasks/CommentsTask.js
@@ -3,53 +3,30 @@ import BaseTask       from './BaseTask';
 import CommentRequest from '../api/amqpRequests/CommentRequest';
 import BaseApiError   from '../api/errors/BaseApiError';
 
-/**
- * @property {TaskDocument} parentTask
- * @property {VkApi} vkApi
- */
 class CommentsTask extends BaseTask {
-	constructor({ parentTask, vkApi, ...args }) {
-		super(args);
-		this.parentTask = parentTask;
-		this.vkApi      = vkApi;
-	}
-	
 	async handle() {
 		const Task = mongoose.model('Task');
 		
-		this.parentTask.subTasks.push(this.taskDocument);
-		await this.taskDocument.save();
-		const request = new CommentRequest(this.config, {
-			postLink     : this.taskDocument.postLink,
-			commentsCount: this.taskDocument.commentsCount,
-		});
-		this.logger.info({ request });
-		
-		(async () => {
-			try {
-				const result = await this.rpcClient.call(request);
-				if (result.error) {
-					throw result.error;
-				}
-			} catch (error) {
-				const wrapedError = new BaseApiError(error.message, 500).combine(error);
-				this.logger.error({ error });
-				this.taskDocument._error  = wrapedError.toObject();
-				// @TODO: Вынести в алерт в либы
-				await this.vkApi.apiRequest('messages.send', {
-					chat_id: this.config.get('vkAlert.chat_id'),
-					message: JSON.stringify(wrapedError.toObject(), null, 2),
-				});
-			}
+		try {
+			const request = new CommentRequest(this.config, {
+				postLink     : this.taskDocument.postLink,
+				commentsCount: this.taskDocument.commentsCount,
+			});
+			this.logger.info({ request });
 			
+			const result = await this.rpcClient.call(request);
+			if (result.error) {
+				throw result.error;
+			}
+		} catch (error) {
+			const wrapedError = new BaseApiError(error.message, 500).combine(error);
+			this.logger.error({ error });
+			this.taskDocument._error  = wrapedError.toObject();
+			throw wrapedError;
+		} finally {
 			this.taskDocument.status = Task.status.finished;
 			await this.taskDocument.save();
-			
-			// Сохраняем родительскую задачу, что бы проставить статусы
-			this.parentTask.lastLikedAt = new Date();
-			this.parentTask.status      = Task.status.waiting;
-			await this.parentTask.save();
-		})();
+		}
 	}
 }
 
