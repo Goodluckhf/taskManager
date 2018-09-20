fa4fa6a6020b28260effdd1e13c3c0389d29a084
Konstantin <goodluckhf@yandex.ru>
2018-09-20 14:12:39 +0300

    feat(services/api/tasks/LikesTask.js): Добавил поле сервиса через который ставятся лайки

--- services/api/tasks/LikesTask.js
+++ services/api/tasks/LikesTask.js
@@ -1,24 +1,27 @@
-import mongoose from 'mongoose';
-
+import mongoose     from 'mongoose';
 import BaseTask     from './BaseTask';
 import LikeRequest  from '../api/amqpRequests/LikeRequest';
 import BaseApiError from '../api/errors/BaseApiError';
 
+/**
+ * @property {String} service
+ */
 class LikesTask extends BaseTask {
+	constructor({ service, ...args }) {
+		super(args);
+		this.service = service;
+	}
+	
 	async handle() {
 		const Task = mongoose.model('Task');
-		
 		try {
-			const request = new LikeRequest(this.config, {
+			const request = new LikeRequest(this.service, this.config, {
 				postLink  : this.taskDocument.postLink,
 				likesCount: this.taskDocument.likesCount,
 			});
 			this.logger.info({ request });
 			
-			const result = await this.rpcClient.call(request);
-			if (result.error) {
-				throw result.error;
-			}
+			await this.rpcClient.call(request);
 		} catch (error) {
 			const wrapedError = new BaseApiError(error.message, 500).combine(error);
 			this.logger.error({ error });
