d1f1d751e962aa7ef699564ea96c2c43e8613a9d
Konstantin <goodluckhf@yandex.ru>
2018-09-23 22:59:29 +0300

    feat(services/taskConsumer/responses/LikesCheckResponse.js): Добавил обработчик задачи проверки лайков

---
+++ services/taskConsumer/responses/LikesCheckResponse.js
@@ -0,0 +1,39 @@
+import Response         from '../../../lib/amqp/Response';
+import { postIdByLink } from '../../../lib/helper';
+
+/**
+ * @property {String} token
+ * @property {VkApi} vkApi
+ */
+class LikesCheckResponse extends Response {
+	constructor({ vkApi, ...args }) {
+		super(args);
+		this.vkApi = vkApi;
+	}
+	
+	/**
+	 * @return {String}
+	 */
+	// eslint-disable-next-line class-methods-use-this,
+	get method() {
+		return 'checkLikes';
+	}
+	
+	async process({ postLink, likeCount }) {
+		this.logger.info({ postLink, likeCount });
+		const postId = postIdByLink(postLink);
+		const { response: [post] } = await this.vkApi.apiRequest('wall.getById', {
+			posts             : postId,
+			copy_history_depth: 1,
+		});
+		
+		if (post.likes.count < likeCount) {
+			const error = new Error('there is not likes');
+			error.likesCount = post.likes.count;
+			error.postLink   = postLink;
+			throw error;
+		}
+	}
+}
+
+export default LikesCheckResponse;
