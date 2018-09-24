e1884c838ec8e0f41e5ae3183e593277e3a97652
Konstantin <goodluckhf@yandex.ru>
2018-09-24 18:27:58 +0300

    feat(services/api/api/amqpRequests/CommentCheckRequest.js): Добавил amqp Request для проверки комментов

---
+++ services/api/api/amqpRequests/CommentCheckRequest.js
@@ -0,0 +1,17 @@
+import Request from '../../../../lib/amqp/Request';
+
+class CommentCheckRequest extends Request {
+	constructor(config, args) {
+		const queue   = config.get('tasksQueue.name');
+		const timeout = config.get('tasksQueue.timeout');
+		const method  = 'checkComments';
+		super({
+			args,
+			queue,
+			method,
+			timeout,
+		});
+	}
+}
+
+export default CommentCheckRequest;
