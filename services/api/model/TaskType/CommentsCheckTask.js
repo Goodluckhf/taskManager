3bc8a5855b15fd7d4343aa364c13bad8a6c418f3
Konstantin <goodluckhf@yandex.ru>
2018-09-24 18:28:25 +0300

    feat(services/api/model/TaskType/CommentsCheckTask.js): Добавил модель

---
+++ services/api/model/TaskType/CommentsCheckTask.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const commentsCheckTaskSchema = new mongoose.Schema({
+	postLink: {
+		type    : String,
+		required: true,
+	},
+	
+	commentsCount: {
+		type    : Number,
+		required: true,
+	},
+	
+	serviceIndex: {
+		type    : Number,
+		required: true,
+	},
+});
+
+/**
+ * @property {String} postLink
+ * @property {Number} commentsCount
+ */
+export class CommentsCheckTaskDocument {
+	/**
+	 * @param {Object} opts
+	 * @param {Number} opts.commentsCount
+	 * @param {String} opts.postLink
+	 * @param {TaskDocument} opts.parentTask
+	 * @param {Date} opts.startAt
+	 * @param {Number} opts.serviceIndex
+	 * @return {CommentsCheckTaskDocument}
+	 */
+	static createInstance(opts) {
+		const baseTask = mongoose.model('Task').createInstance(this, opts);
+		baseTask.commentsCount = opts.commentsCount;
+		baseTask.postLink      = opts.postLink;
+		baseTask.parentTask    = opts.parentTask;
+		baseTask.startAt       = opts.startAt;
+		baseTask.serviceIndex  = opts.serviceIndex;
+		return baseTask;
+	}
+}
+
+commentsCheckTaskSchema.loadClass(CommentsCheckTaskDocument);
+
+export default commentsCheckTaskSchema;
