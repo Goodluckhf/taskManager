5ae0f2274835ea53224fef0a40ea5d4a6e5ecf54
Konstantin <goodluckhf@yandex.ru>
2018-09-25 23:32:06 +0300

    feat(services/api/tasks/CheckWallBanTask.js): Добавил задачу обработки слежки

---
+++ services/api/tasks/CheckWallBanTask.js
@@ -0,0 +1,31 @@
+import mongoose            from 'mongoose';
+import BaseTask            from './BaseTask';
+import BaseApiError        from '../api/errors/BaseApiError';
+import CheckWallBanRequest from '../api/amqpRequests/CheckWallBanRequest';
+
+class CheckWallBanTask extends BaseTask {
+	async handle() {
+		const Task  = mongoose.model('Task');
+		try {
+			await this.taskDocument.populate('group').execPopulate();
+			
+			const request = new CheckWallBanRequest(this.config, {
+				postCount: this.taskDocument.postCount,
+				link     : this.taskDocument.link,
+			});
+			this.logger.info({ request });
+			
+			await this.rpcClient.call(request);
+			this.taskDocument.status = Task.status.waiting;
+		} catch (error) {
+			const wrapedError = new BaseApiError(error.message, 500).combine(error);
+			this.taskDocument._error = wrapedError.toObject();
+			this.taskDocument.status = Task.status.finished;
+			throw wrapedError;
+		} finally {
+			await this.taskDocument.save();
+		}
+	}
+}
+
+export default CheckWallBanTask;
