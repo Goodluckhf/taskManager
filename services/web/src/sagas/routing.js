a194d3985c020eb6ca660989964f9fcbecc2a5ae
Konstantin <goodluckhf@yandex.ru>
2018-09-26 01:05:45 +0300

    feat(services/web/src/sagas/routing.js): Добавил сагу для роута слежки

--- services/web/src/sagas/routing.js
+++ services/web/src/sagas/routing.js
@@ -2,6 +2,7 @@ import { takeEvery, put }  from 'redux-saga/effects';
 import { LOCATION_CHANGE } from 'connected-react-router';
 import { listRequest as groupListRequest }     from '../actions/groups';
 import { listRequest as autolikesListRequest } from '../actions/autolikes';
+import { listRequest as wallSeekListRequest }  from '../actions/wallSeek';
 
 export default function* () {
 	yield takeEvery(LOCATION_CHANGE, function* ({ payload: { location } }) {
@@ -12,6 +13,10 @@ export default function* () {
 		
 		if (location.pathname === '/autolikes') {
 			yield put(autolikesListRequest());
+		}
+		
+		if (location.pathname === '/wallseek') {
+			yield put(wallSeekListRequest());
 		}
 	});
 }
