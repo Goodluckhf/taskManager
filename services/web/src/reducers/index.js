75752f6ae8e88277a7b4ff6a7115d44944841c85
Konstantin <goodluckhf@yandex.ru>
2018-09-26 01:04:28 +0300

    feat(services/web/src/reducers/wallSeek.js): Добавил reducer для задачи слежки

--- services/web/src/reducers/index.js
+++ services/web/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { routes }    from '../store/initialState';
 import groupPage     from './groups';
 import autoLikesPage from './autolikes';
+import wallSeekPage  from './wallSeek';
 import fatalError    from './fatalError';
 import { loaderReducer, errorReducer } from '../lib/loader';
 
@@ -9,6 +10,7 @@ export default combineReducers({
 	routes: (state = routes) => state,
 	groupPage,
 	autoLikesPage,
+	wallSeekPage,
 	fatalError,
 	loader: loaderReducer,
 	error : errorReducer,
---
+++ services/web/src/reducers/wallSeek.js
@@ -0,0 +1,32 @@
+import { fromJS } from 'immutable';
+
+import { wallSeekPage } from '../store/initialState';
+import {
+	CREATE_SUCCESS,
+	LIST_SUCCESS, REMOVE_SUCCESS,
+} from '../actions/wallSeek';
+
+//eslint-disable-next-line
+export default (state = wallSeekPage, { type, payload }) => {
+	if (type === CREATE_SUCCESS) {
+		console.log(payload);
+		return state.updateIn(
+			['list', 'items'],
+			items => items.push(fromJS(payload.task)),
+		);
+	}
+	
+	if (type === LIST_SUCCESS) {
+		return state.updateIn(
+			['list', 'items'],
+			() => fromJS(payload.tasks),
+		);
+	}
+	
+	if (type === REMOVE_SUCCESS) {
+		const index = state.getIn(['list', 'items']).findIndex(item => item.get('_id') === payload.id);
+		return state.deleteIn(['list', 'items', index]);
+	}
+	
+	return state;
+};
