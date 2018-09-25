0e4104f7b8fd69d027e799511c141fd052158eff
Konstantin <goodluckhf@yandex.ru>
2018-09-26 01:05:13 +0300

    feat(services/web/src/sagas/wallSeek.js): ДОбавил сагу для создания, списка и удаления задачи слежки

--- services/web/src/sagas/index.js
+++ services/web/src/sagas/index.js
@@ -1,10 +1,12 @@
 import { fork }  from 'redux-saga/effects';
 import groupSaga from './group';
 import autolikes from './autolikes';
+import wallSeek  from './wallSeek';
 import routing   from './routing';
 
 export default function* rootSage() {
 	yield fork(groupSaga);
 	yield fork(autolikes);
 	yield fork(routing);
+	yield fork(wallSeek);
 }
---
+++ services/web/src/sagas/wallSeek.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+import { takeEvery, put, call } from 'redux-saga/effects';
+import {
+	CREATE_REQUEST, LIST_REQUEST, REMOVE_REQUEST,
+	createSuccess, createFailure,
+	listSuccess, removeSuccess, removeFailure,
+} from '../actions/wallSeek';
+import { fatalError } from '../actions/fatalError';
+
+export default function* () {
+	yield takeEvery(CREATE_REQUEST, function* ({ payload: data }) {
+		try {
+			const { data: result } = yield call(axios.post, '/api/wallseek', data);
+			if (!result.success) {
+				yield put(createFailure(result.data));
+				return;
+			}
+			
+			yield put(createSuccess(result.data));
+		} catch (error) {
+			if (error.response && error.response.data) {
+				yield put(createFailure(error.response.data));
+				return;
+			}
+			
+			yield put(fatalError(error));
+		}
+	});
+	
+	yield takeEvery(LIST_REQUEST, function* () {
+		try {
+			const { data: result } = yield call(axios.get, '/api/wallseek');
+			if (!result.success) {
+				yield put(fatalError(result.data));
+				return;
+			}
+			
+			yield put(listSuccess(result.data));
+		} catch (error) {
+			yield put(fatalError(error));
+		}
+	});
+	
+	
+	yield takeEvery(REMOVE_REQUEST, function* ({ payload: { id } }) {
+		try {
+			yield call(axios.delete, `/api/task/${id}`);
+			yield put(removeSuccess(id));
+		} catch (error) {
+			if (error.response && error.response.data) {
+				yield put(removeFailure(error.response.data, id));
+				return;
+			}
+			
+			yield put(fatalError(error));
+		}
+	});
+}
