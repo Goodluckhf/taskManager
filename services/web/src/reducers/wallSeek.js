4a83bd6141d5143a52e6d36aeef16f729f0185b7
Konstantin <goodluckhf@yandex.ru>
2018-09-28 00:32:16 +0300

    feat(services/web/src/reducers/wallSeek.js): Добавил reducer для возобновления задачи

--- services/web/src/reducers/wallSeek.js
+++ services/web/src/reducers/wallSeek.js
@@ -2,8 +2,8 @@ import { fromJS } from 'immutable';
 
 import { wallSeekPage } from '../store/initialState';
 import {
-	CREATE_SUCCESS,
-	LIST_SUCCESS, REMOVE_SUCCESS,
+	CREATE_SUCCESS, LIST_SUCCESS,
+	REMOVE_SUCCESS, RESUME_SUCCESS,
 } from '../actions/wallSeek';
 
 //eslint-disable-next-line
@@ -26,6 +26,14 @@ export default (state = wallSeekPage, { type, payload }) => {
 	if (type === REMOVE_SUCCESS) {
 		const index = state.getIn(['list', 'items']).findIndex(item => item.get('_id') === payload.id);
 		return state.deleteIn(['list', 'items', index]);
+	}
+	
+	if (type === RESUME_SUCCESS) {
+		const index = state.getIn(['list', 'items']).findIndex(item => item.get('_id') === payload.id);
+		return state.updateIn(
+			['list', 'items', index],
+			task => task.set('status', 0),
+		);
 	}
 	
 	return state;
