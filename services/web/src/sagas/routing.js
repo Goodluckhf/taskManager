1289ea8f79823c131b0e35cb4f27c6a689e08919
Konstantin <goodluckhf@yandex.ru>
2018-09-28 12:06:10 +0300

    feat(services/web/src/sagas/routing.js): Добавил обновление данных по интервалу

--- services/web/src/sagas/routing.js
+++ services/web/src/sagas/routing.js
@@ -1,23 +1,37 @@
-import { takeEvery, put }  from 'redux-saga/effects';
+import { takeEvery, put, call, fork, cancel }  from 'redux-saga/effects';
+import { delay } from 'redux-saga';
 import { LOCATION_CHANGE } from 'connected-react-router';
 import { listRequest as groupListRequest }     from '../actions/groups';
 import { listRequest as autolikesListRequest } from '../actions/autolikes';
 import { listRequest as wallSeekListRequest }  from '../actions/wallSeek';
 
+const mapperPathToActionCreator = {
+	'/groups'   : groupListRequest,
+	'/autolikes': autolikesListRequest,
+	'/wallseek' : wallSeekListRequest,
+};
+
+const loopUpdate = function* (actionCreator, interval) {
+	while (true) {
+		yield put(actionCreator());
+		yield call(delay, interval);
+	}
+};
+
 export default function* () {
+	const interval = 5 * 1000;
+	let currentLoopTask = null;
 	yield takeEvery(LOCATION_CHANGE, function* ({ payload: { location } }) {
-		if (location.pathname === '/groups') {
-			yield put(groupListRequest());
+		if (currentLoopTask) {
+			yield cancel(currentLoopTask);
+		}
+		
+		const actionCreator = mapperPathToActionCreator[location.pathname];
+		if (!actionCreator) {
 			return;
 		}
 		
-		if (location.pathname === '/autolikes') {
-			yield put(autolikesListRequest());
-		}
-		
-		if (location.pathname === '/wallseek') {
-			yield put(wallSeekListRequest());
-		}
+		currentLoopTask = yield fork(loopUpdate, actionCreator, interval);
 	});
 }
 
