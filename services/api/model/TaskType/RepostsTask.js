e67843e797c2f90b58dea3cd4c00e74edba48cb0
Konstantin <goodluckhf@yandex.ru>
2018-09-30 01:32:46 +0300

    feat(services/api/model/TaskType/RepostsTask.js): Добавил модель накрутки репостов

---
+++ services/api/model/TaskType/RepostsTask.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const repostsTaskSchema = new mongoose.Schema({
+	postLink: {
+		type    : String,
+		required: true,
+	},
+	
+	repostsCount: {
+		type    : Number,
+		required: true,
+	},
+	
+	service: {
+		type    : String,
+		required: true,
+	},
+});
+
+/**
+ * @property {String} postLink
+ * @property {Number} repostsCount
+ * @property {String} service
+ * @property {TaskDocument} parentTask
+ */
+export class RepostsTaskDocument {
+	/**
+	 * @param {Object} opts
+	 * @param {Number} opts.repostsCount
+	 * @param {String} opts.postLink
+	 * @param {String} opts.service
+	 * @param {TaskDocument} opts.parentTask
+	 * @return {RepostsTaskDocument}
+	 */
+	static createInstance(opts) {
+		const baseTask = mongoose.model('Task').createInstance(this, opts);
+		baseTask.repostsCount  = opts.repostsCount;
+		baseTask.postLink      = opts.postLink;
+		baseTask.service       = opts.service;
+		baseTask.parentTask    = opts.parentTask || null;
+		return baseTask;
+	}
+}
+
+repostsTaskSchema.loadClass(RepostsTaskDocument);
+
+export default repostsTaskSchema;
