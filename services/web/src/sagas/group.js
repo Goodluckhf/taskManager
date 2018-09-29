22ca682bfc080360ecb82d63065c9c141885fb88
Konstantin <goodluckhf@yandex.ru>
2018-09-29 11:59:30 +0300

    feat(services/web/src/sagas/group.js): Добавил метод update

--- services/web/src/sagas/group.js
+++ services/web/src/sagas/group.js
@@ -1,13 +1,23 @@
-import { takeEvery, put, call } from 'redux-saga/effects';
+import { takeEvery, put, call, select } from 'redux-saga/effects';
 import axios from 'axios';
 
 import {
 	CREATE_REQUEST, LIST_REQUEST,
 	CHANGE_IS_TARGET, FILTER_CHANGE_REQUEST,
 	createFailure, createSuccess, listSuccess,
+	listRequest,
 } from '../actions/groups';
 
 import { fatalError } from '../actions/fatalError';
+
+export const list = function* (filterState) {
+	try {
+		const { data: result } = yield call(axios.get, '/api/groups', { params: filterState });
+		yield put(listSuccess(result.data));
+	} catch (error) {
+		yield put(fatalError(error));
+	}
+};
 
 export default function* () {
 	yield takeEvery(CREATE_REQUEST, function* ({ payload: data }) {
@@ -29,13 +39,8 @@ export default function* () {
 		}
 	});
 	
-	yield takeEvery(LIST_REQUEST, function* () {
-		try {
-			const { data: result } = yield call(axios.get, '/api/groups');
-			yield put(listSuccess(result.data));
-		} catch (error) {
-			yield put(fatalError(error));
-		}
+	yield takeEvery(LIST_REQUEST, function* ({ payload: { filterState } }) {
+		yield list(filterState);
 	});
 	
 	yield takeEvery(CHANGE_IS_TARGET, function* ({ payload: { id, isTarget } }) {
@@ -47,11 +52,14 @@ export default function* () {
 	});
 	
 	yield takeEvery(FILTER_CHANGE_REQUEST, function* ({ payload: { filterState } }) {
-		try {
-			const { data: result } = yield call(axios.get, '/api/groups', { params: filterState });
-			yield put(listSuccess(result.data));
-		} catch (error) {
-			yield put(fatalError(error));
-		}
+		yield list(filterState);
 	});
 }
+
+export const update = function* () {
+	const filter = yield select(state => (
+		state.groupPage.getIn(['list', 'filter'])
+	));
+	
+	yield put(listRequest(filter.toJS()));
+};
