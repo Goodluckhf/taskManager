e59bb77a78ded1928f76631435fbdf2f43a6f8c7
Konstantin <goodluckhf@yandex.ru>
2018-09-30 01:35:59 +0300

    feat(services/api/api/amqpRequests/RepostRequest.js): Добавил amqp Request для накртуки репостов

---
+++ services/api/api/amqpRequests/RepostRequest.js
@@ -0,0 +1,17 @@
+import Request from '../../../../lib/amqp/Request';
+
+class RepostRequest extends Request {
+	constructor(service, config, args) {
+		const queue   = config.get('tasksQueue.name');
+		const timeout = config.get('tasksQueue.timeout');
+		const method  = `setReposts_${service}`;
+		super({
+			args,
+			queue,
+			method,
+			timeout,
+		});
+	}
+}
+
+export default RepostRequest;
