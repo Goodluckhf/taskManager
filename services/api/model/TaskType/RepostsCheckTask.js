b324c3f057d1b5d4ff70d742424955f51fc5f91b
Konstantin <goodluckhf@yandex.ru>
2018-09-30 01:33:56 +0300

    feat(services/api/model/TaskType/RepostsCheckTask.js): Добавил модель для проверки накрутки репостов

---
+++ services/api/model/TaskType/RepostsCheckTask.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const repostsCheckTaskSchema = new mongoose.Schema({
+	postLink: {
+		type    : String,
+		required: true,
+	},
+	
+	repostsCount: {
+		type    : Number,
+		required: true,
+	},
+	
+	serviceIndex: {
+		type    : Number,
+		required: true,
+	},
+});
+
+/**
+ * @property {String} postLink
+ * @property {Number} repostsCount
+ */
+export class RepostsCheckTaskDocument {
+	/**
+	 * @param {Object} opts
+	 * @param {Number} opts.repostsCount
+	 * @param {String} opts.postLink
+	 * @param {TaskDocument} opts.parentTask
+	 * @param {Date} opts.startAt
+	 * @param {Number} opts.serviceIndex
+	 * @return {RepostsCheckTaskDocument}
+	 */
+	static createInstance(opts) {
+		const baseTask = mongoose.model('Task').createInstance(this, opts);
+		baseTask.repostsCount  = opts.repostsCount;
+		baseTask.postLink      = opts.postLink;
+		baseTask.parentTask    = opts.parentTask;
+		baseTask.startAt       = opts.startAt;
+		baseTask.serviceIndex  = opts.serviceIndex;
+		return baseTask;
+	}
+}
+
+repostsCheckTaskSchema.loadClass(RepostsCheckTaskDocument);
+
+export default repostsCheckTaskSchema;
