b8cf6c87d0df4f66d8fb97c1c6fe0efa0b0be968
Konstantin <goodluckhf@yandex.ru>
2018-09-30 01:33:15 +0300

    feat(services/api/model/TaskType/RepostsCommon.js): Добавил модель для общей задачи накртуки репостов

---
+++ services/api/model/TaskType/RepostsCommon.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+
+const repostsCommonTaskSchema = new mongoose.Schema({
+	postLink: {
+		type    : String,
+		required: true,
+	},
+	
+	repostsCount: {
+		type    : Number,
+		required: true,
+	},
+});
+
+/**
+ * @property {String} postLink
+ * @property {Number} repostsCount
+ */
+export class RepostsCommonTaskDocument {
+	/**
+	 * @param {Object} opts
+	 * @param {Number} opts.repostsCount
+	 * @param {String} opts.postLink
+	 * @param {TaskDocument} opts.parentTask
+	 * @return {RepostsCommonTaskDocument}
+	 */
+	static createInstance(opts) {
+		const baseTask = mongoose.model('Task').createInstance(this, opts);
+		baseTask.repostsCount  = opts.repostsCount;
+		baseTask.postLink      = opts.postLink;
+		baseTask.parentTask    = opts.parentTask || null;
+		return baseTask;
+	}
+	
+	/**
+	 * @param {Object} opts
+	 * @param {Number} opts.repostsCount
+	 * @param {String} opts.postLink
+	 * @return {RepostsCommonTaskDocument}
+	 */
+	// eslint-disable-next-line object-curly-newline
+	fill({ repostsCount, postLink }) {
+		if (repostsCount) {
+			this.repostsCount = repostsCount;
+		}
+		
+		if (postLink) {
+			this.postLink = postLink;
+		}
+		
+		return this;
+	}
+}
+
+repostsCommonTaskSchema.loadClass(RepostsCommonTaskDocument);
+
+export default repostsCommonTaskSchema;
