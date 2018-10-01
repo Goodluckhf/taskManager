27cdb2da8a5538cecdf91109aed4b6f86fe7045f
Konstantin <goodluckhf@yandex.ru>
2018-10-01 14:43:45 +0300

    feat(services/web/src/actions/auth.js): Добавил action'ы для авторизации

--- services/web/src/actions/auth.js
+++ services/web/src/actions/auth.js
@@ -3,3 +3,21 @@ export const needLogin  = lastRoute => ({
 	type   : NEED_LOGIN,
 	payload: { lastRoute },
 });
+
+export const LOGIN_REQUEST = 'AUTH__LOGIN_REQUEST';
+export const loginRequest  = credentials => ({
+	type   : LOGIN_REQUEST,
+	payload: credentials,
+});
+
+export const LOGIN_FAILURE = 'AUTH__LOGIN_FAILURE';
+export const loginFailure = error => ({
+	type   : LOGIN_FAILURE,
+	payload: { error },
+});
+
+export const LOGIN_SUCCESS = 'AUTH__LOGIN_SUCCESS';
+export const loginSuccess = data => ({
+	type   : LOGIN_SUCCESS,
+	payload: data,
+});
