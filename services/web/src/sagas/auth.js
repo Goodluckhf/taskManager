35cbda87a2be7f817df27e39af71de8633606e36
Konstantin <goodluckhf@yandex.ru>
2018-10-01 14:44:29 +0300

    feat(services/web/src/sagas/auth.js): Добавил сагу для авторизации

--- services/web/src/sagas/auth.js
+++ services/web/src/sagas/auth.js
@@ -1,6 +1,16 @@
-import { select, put, takeEvery, fork } from 'redux-saga/effects';
-import { push }                  from 'connected-react-router';
-import { NEED_LOGIN, needLogin } from '../actions/auth';
+import {
+	select, put,
+	takeEvery, fork, call,
+}               from 'redux-saga/effects';
+import axios    from 'axios';
+import { push } from 'connected-react-router';
+
+import {
+	LOGIN_REQUEST, NEED_LOGIN,
+	loginFailure, loginSuccess,
+	needLogin,
+} from '../actions/auth';
+
 
 export const checkLogin = function* () {
 	const jwt        = yield select(state => state.auth.get('jwt'));
@@ -8,23 +18,47 @@ export const checkLogin = function* () {
 	
 	if (currentRoute === '/login') {
 		currentRoute = '/groups';
+		if (jwt) {
+			yield put(push(currentRoute));
+			return jwt;
+		}
 	}
 	
 	if (!jwt) {
 		yield put(needLogin(currentRoute));
+		return false;
 	}
+	
+	return jwt;
 };
 
 export default function* () {
 	yield fork(checkLogin);
 	
-	yield takeEvery(NEED_LOGIN, function* () {
-		const currentRoute = yield select(state => state.router.location.pathname);
+	yield takeEvery(NEED_LOGIN, function* (currentRoute) {
 		if (currentRoute === '/login') {
 			return;
 		}
 		
 		yield put(push('/login'));
+	});
+	
+	yield takeEvery(LOGIN_REQUEST, function* ({ payload: data }) {
+		try {
+			const { data: { data: response } } = yield call(axios.post, '/api/login', data);
+			yield put(loginSuccess(response));
+			const lastRoute = yield select(state => state.auth.get('lastRoute'));
+			// eslint-disable-next-line no-undef
+			window.localStorage.setItem('tasks_jwt', response.token);
+			yield put(push(lastRoute));
+		} catch (error) {
+			if (error.response && error.response.data) {
+				yield put(loginFailure(error.response.data));
+				return;
+			}
+			
+			yield put(loginFailure(error));
+		}
 	});
 }
 
