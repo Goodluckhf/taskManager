0d2120a47b553d1d693e389795a3a4bc6ed9a626
Konstantin <goodluckhf@yandex.ru>
2018-10-02 13:41:58 +0300

    feat(services/web/src/views/pages/Register.js): ДОбавил страницу регистрации

---
+++ services/web/src/views/pages/Register.js
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react';
+import { connect }              from 'react-redux';
+import propTypes                from 'prop-types';
+
+import RegisterForm        from '../components/auth/RegisterForm';
+import { loaderSelector }  from '../../lib/loader';
+import { registerRequest } from '../../actions/auth';
+
+class Register extends PureComponent {
+	static propTypes = {
+		register    : propTypes.func.isRequired,
+		registerForm: propTypes.object.isRequired,
+	};
+	
+	render() {
+		return (
+			<RegisterForm
+				loading={this.props.registerForm.get('loading')}
+				error={this.props.registerForm.get('error')}
+				register={this.props.register}
+			/>
+		);
+	}
+}
+
+const mapDispatchToProps = dispatch => ({
+	register: data => dispatch(registerRequest(data)),
+});
+
+const mapStateToProps = state => ({
+	registerForm: loaderSelector({ AUTH__REGISTER: 'loading' }, 'auth', state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
--- services/web/src/views/pages/index.js
+++ services/web/src/views/pages/index.js
@@ -3,6 +3,7 @@ import Groups    from './Groups';
 import AutoLikes from './AutoLikes';
 import WallSeek  from './WallSeek';
 import Login     from './Login';
+import Register     from './Register';
 
 // eslint-disable-next-line import/prefer-default-export
-export { Page404, Groups, AutoLikes, WallSeek, Login };
+export { Page404, Groups, AutoLikes, WallSeek, Login, Register };
