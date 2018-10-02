df3110145ef8bd04b38bfc8c14bd1bd9359c2832
Konstantin <goodluckhf@yandex.ru>
2018-10-02 23:49:28 +0300

    refactor(services/web/src/sagas/routing.js): Переписал на возможность отправки запроса при переходе по роуты без лупа

--- services/web/src/sagas/routing.js
+++ services/web/src/sagas/routing.js
@@ -1,14 +1,28 @@
 import { takeEvery, call, fork, cancel } from 'redux-saga/effects';
-import { delay }                         from 'redux-saga';
-import { LOCATION_CHANGE }               from 'connected-react-router';
-import { update as updateGroup }         from './group';
-import { update as updateAutolikes }     from './autolikes';
-import { update as updateWallSeek }      from './wallSeek';
+import { delay }                     from 'redux-saga';
+import { LOCATION_CHANGE }           from 'connected-react-router';
+import { update as updateGroup }     from './group';
+import { update as updateAutolikes } from './autolikes';
+import { update as updateWallSeek }  from './wallSeek';
+import { getUserData }               from './auth';
 
 const mapperPathToUpdateFunction = {
-	'/groups'   : updateGroup,
-	'/autolikes': updateAutolikes,
-	'/wallseek' : updateWallSeek,
+	'/groups': {
+		function: updateGroup,
+		loop    : true,
+	},
+	'/autolikes': {
+		function: updateAutolikes,
+		loop    : true,
+	},
+	'/wallseek': {
+		function: updateWallSeek,
+		loop    : true,
+	},
+	'/settings': {
+		function: getUserData,
+		loop    : false,
+	},
 };
 
 const loopUpdate = function* (updateFunction, interval) {
@@ -24,14 +38,20 @@ export default function* () {
 	yield takeEvery(LOCATION_CHANGE, function* ({ payload: { location } }) {
 		if (currentLoopTask) {
 			yield cancel(currentLoopTask);
+			currentLoopTask = null;
 		}
 		
-		const updateFunction = mapperPathToUpdateFunction[location.pathname];
-		if (!updateFunction) {
+		const updateOption = mapperPathToUpdateFunction[location.pathname];
+		if (!updateOption) {
 			return;
 		}
 		
-		currentLoopTask = yield fork(loopUpdate, updateFunction, interval);
+		if (!updateOption.loop) {
+			yield updateOption.function();
+			return;
+		}
+		
+		currentLoopTask = yield fork(loopUpdate, updateOption.function, interval);
 	});
 }
 
