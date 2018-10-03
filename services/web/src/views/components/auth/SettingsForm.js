e6605b02c3cc9b1fca17ea624262a2bf4b9255aa
Konstantin <goodluckhf@yandex.ru>
2018-10-03 23:50:27 +0300

    feat(services/web/src/views/components/auth/SettingsForm.js): Форма создания чата для алертов

---
+++ services/web/src/views/components/auth/SettingsForm.js
@@ -0,0 +1,86 @@
+import React, { PureComponent } from 'react';
+import propTypes from 'prop-types';
+import {
+	Card, CardHeader, CardBody,
+	Form as BootstrapForm, FormGroup,
+	Label, Input, CardFooter, Row, Col,
+} from 'reactstrap';
+
+import LoadingButton, { S }  from '../ui/LoadingButton';
+import ApiError              from '../ui/ApiError';
+
+class Form extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			vkLink: '',
+		};
+	}
+	static propTypes = {
+		chatId         : propTypes.number,
+		error          : propTypes.object,
+		vkLink         : propTypes.string.isRequired,
+		loadingChat    : propTypes.bool.isRequired,
+		loadingUserData: propTypes.bool.isRequired,
+		createChat     : propTypes.func.isRequired,
+		systemVkLink   : propTypes.string.isRequired,
+	};
+	
+	
+	onLinkChange = (e) => {
+		this.setState({ vkLink: e.target.value.trim() });
+	};
+	
+	onClick = () => {
+		this.props.createChat(this.state.vkLink);
+	};
+	
+	render() {
+		return (
+			<Card>
+				<CardHeader><b>Настройки аккаунта</b></CardHeader>
+				<CardBody>
+					<p>Заполните информацию, что бы приходили алерты в чат vk.com</p>
+					<BootstrapForm>
+						<FormGroup>
+							<Row>
+								<Col sm='12'>
+									<p>
+										{/*eslint-disable-next-line max-len*/}
+										1) Добавьте в друзья пользователя: <a href={this.props.systemVkLink}>{this.props.systemVkLink}</a><br/>
+										2) Вставьте сслку на профиль, куда вы хотите, что бы приходили оповещания<br/>
+										3) Нажмите на кнопку создать чат <br/>
+									</p>
+								</Col>
+								{this.props.loadingUserData ? 'Загружаю...' :
+									<Col sm='12'>
+										<Label>Ссылка на профиль vk.com:</Label>
+										{!this.props.chatId &&
+											<Input onChange={this.onLinkChange} type='text' placeholder='https://vk.com/id123'/>
+										}
+										{this.props.chatId && <a rel='noopener noreferrer' target='_blank' href={this.props.vkLink}>{this.props.vkLink}</a>}
+									</Col>
+								}
+							</Row>
+						</FormGroup>
+						{this.props.error ? <ApiError style={{ marginTop: '24px' }} error={this.props.error}/> : ''}
+					</BootstrapForm>
+				</CardBody>
+				{!this.props.loadingUserData && !this.props.chatId &&
+					<CardFooter>
+						<LoadingButton
+							data-size={S}
+							data-color='green'
+							loading={this.props.loadingChat}
+							onClick={this.onClick}
+						>
+							Создать чат
+						</LoadingButton>
+					</CardFooter>
+				}
+			</Card>
+		);
+	}
+}
+
+export default Form;
---
+++ services/web/src/views/pages/Settings.js
@@ -0,0 +1,55 @@
+import React, { PureComponent } from 'react';
+import Immutable                from 'immutable';
+import { connect }              from 'react-redux';
+import propTypes                from 'prop-types';
+import { Container }            from 'reactstrap';
+import Layout                   from '../layout/Layout';
+import SettingsForm             from '../components/auth/SettingsForm';
+
+import { loaderSelector }    from '../../lib/loader';
+import { createChatRequest } from '../../actions/auth';
+
+class Settings extends PureComponent {
+	static propTypes = {
+		chatId      : propTypes.number,
+		error       : propTypes.object,
+		vkLink      : propTypes.string.isRequired,
+		systemVkLink: propTypes.string.isRequired,
+		createChat  : propTypes.func.isRequired,
+		loaders     : propTypes.instanceOf(Immutable.Map),
+	};
+	
+	render() {
+		return (
+			<Layout>
+				<Container fluid={true}>
+					<SettingsForm
+						createChat={this.props.createChat}
+						error={this.props.loaders.get('error')}
+						loadingChat={this.props.loaders.get('loadingChat')}
+						loadingUserData={this.props.loaders.get('loadingUserData')}
+						vkLink={this.props.vkLink}
+						systemVkLink={this.props.systemVkLink}
+						chatId={this.props.chatId}
+					/>
+				</Container>
+			</Layout>
+		);
+	}
+}
+
+const mapDispatchToProps = dispatch => ({
+	createChat: data => dispatch(createChatRequest(data)),
+});
+
+const mapStateToProps = state => ({
+	loaders: loaderSelector({
+		AUTH__CREATE_CHAT  : 'loadingChat',
+		AUTH__GET_USER_DATA: 'loadingUserData',
+	}, 'auth', state),
+	chatId      : state.auth.get('chatId'),
+	vkLink      : state.auth.get('vkLink'),
+	systemVkLink: state.auth.get('systemVkLink'),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
