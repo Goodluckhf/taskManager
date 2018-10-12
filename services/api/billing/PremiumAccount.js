1e4c344e97299589fccf9757115ec0b56e5ccca9
Konstantin <goodluckhf@yandex.ru>
2018-10-12 18:40:20 +0300

    feat(services/api/billing/PremiumAccount.js): Класс премиум аккаунта

---
+++ services/api/billing/PremiumAccount.js
@@ -0,0 +1,24 @@
+import BaseAccount   from './BaseAccount';
+import BaseTaskError from '../api/errors/tasks/BaseTaskError';
+
+/**
+ * @property {PremiumUserDocument} user
+ */
+class PremiumAccount extends BaseAccount {
+	/**
+	 * @param {InvoiceDocument | Array.<InvoiceDocument>} invoice
+	 * @return {Boolean}
+	 */
+	// eslint-disable-next-line no-unused-vars
+	canPay(invoice) {
+		const hasAllCredentials = ['likest', 'likePro', 'z1y1x1'].every(service => (
+			this.user.services[service]
+		));
+		
+		if (!hasAllCredentials) {
+			throw new BaseTaskError(new Error('Не все данные для сервисов заполнены'));
+		}
+	}
+}
+
+export default PremiumAccount;
