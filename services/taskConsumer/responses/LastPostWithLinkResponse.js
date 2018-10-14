ee482bb139730a6bad6c8bd0fe0270a50498675c
Konstantin <goodluckhf@yandex.ru>
2018-10-14 16:13:18 +0300

    fix(services/taskConsumer/responses/LastPostWithLinkResponse.js): Фикс от слишком частого обращения к страницам vk

--- services/taskConsumer/responses/LastPostWithLinkResponse.js
+++ services/taskConsumer/responses/LastPostWithLinkResponse.js
@@ -1,6 +1,8 @@
 import cheerio  from 'cheerio';
 import axios    from 'axios';
+import bluebird  from 'bluebird';
 import Response from '../../../lib/amqp/Response';
+import { getRandom } from '../../../lib/helper';
 
 const cleanLink = (link) => {
 	return link.replace(/^(?:https?:\/\/)?(?:www\.)?/, '');
@@ -28,17 +30,22 @@ class LastPostWithLinkResponse extends Response {
 		return 'getLastPostWithLink';
 	}
 	
-	//eslint-disable-next-line class-methods-use-this
-	async process({ groupLink }) {
-		let html;
+	async getHtml(url) {
 		try {
 			const { data } = await axios({
+				url,
 				method : 'get',
-				url    : groupLink,
 				timeout: 5000,
 				headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0' },
 			});
-			html = data;
+			const $ = cheerio.load(data);
+			const fastError = $('.message_page_body');
+			if (fastError.length) {
+				await bluebird.delay(getRandom(300, 2000));
+				return this.getHtml(url);
+			}
+			
+			return $;
 		} catch (_error) {
 			const error = _error;
 			if (error.request) {
@@ -50,9 +57,11 @@ class LastPostWithLinkResponse extends Response {
 			}
 			throw error;
 		}
-		
-		const $ = cheerio.load(html);
-		
+	}
+	
+	//eslint-disable-next-line class-methods-use-this
+	async process({ groupLink }) {
+		const $ = await this.getHtml(groupLink);
 		const $lastPost = $('#page_wall_posts .post').eq(0);
 		if (!$lastPost.length) {
 			throw new Error('Группа пустая');
@@ -70,7 +79,7 @@ class LastPostWithLinkResponse extends Response {
 			result.mentionId = mentionId;
 		} else {
 			const anyLink = $lastPost.find('.wall_text a:not(.wall_post_more)').eq(0);
-			if (anyLink.length) {
+			if (anyLink.length && anyLink.attr('href')) {
 				result.link = parseLink(anyLink.attr('href'));
 			}
 		}
