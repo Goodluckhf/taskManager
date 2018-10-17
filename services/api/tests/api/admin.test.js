40b3628568110cb1e43708eac6349af8031b1c46
Konstantin <goodluckhf@yandex.ru>
2018-10-17 23:59:07 +0300

    test(services/api/tests/api/admin.test.js): Дописал тест для пополнения баланса

--- services/api/tests/api/admin.test.js
+++ services/api/tests/api/admin.test.js
@@ -1,11 +1,17 @@
 import { expect }                    from 'chai';
 import config                        from 'config';
+import _                             from 'lodash';
 import mongoose                      from '../../../../lib/mongoose';
 import AdminApi                      from '../../api/AdminApi';
 import { NotFound, ValidationError } from '../../api/errors';
+import Billing                       from '../../billing/Billing';
 
 const loggerMock = { info() {}, error() {}, warn() {} };
-describe('AdminApi', () => {
+describe('AdminApi', function () {
+	beforeEach(() => {
+		this.config = _.cloneDeep(config);
+	});
+	
 	it('Should throw error if user doesnt exists', async () => {
 		const adminApi = new AdminApi(config, loggerMock);
 		const userId   = mongoose.Types.ObjectId();
@@ -21,12 +27,25 @@ describe('AdminApi', () => {
 	});
 	
 	it('Should increase balance', async () => {
-		const adminApi = new AdminApi(config, loggerMock);
+		const billing  = new Billing(this.config, loggerMock);
+		const adminApi = new AdminApi(billing, config, loggerMock);
 		let user = mongoose.model('AccountUser').createInstance({ email: 'asd', password: 'asd' });
 		await user.save();
 		
 		await expect(adminApi.increaseBalance(user._id, 10)).to.be.fulfilled;
 		user = await mongoose.model('AccountUser').findById(user._id);
 		expect(user.balance).to.be.equals(10);
+	});
+	
+	it('Should create invoice', async () => {
+		const billing  = new Billing(this.config, loggerMock);
+		const adminApi = new AdminApi(billing, config, loggerMock);
+		const user = mongoose.model('AccountUser').createInstance({ email: 'asd', password: 'asd' });
+		await user.save();
+		
+		await expect(adminApi.increaseBalance(user._id, 10)).to.be.fulfilled;
+		const invoice = await mongoose.model('TopUpInvoice').findOne({ user: user._id });
+		
+		expect(invoice.amount).to.be.equals(10);
 	});
 });
