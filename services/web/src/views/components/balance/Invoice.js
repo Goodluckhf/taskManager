f0e137642bc15ab92e94bf9febbc5edea0e222d0
Konstantin <goodluckhf@yandex.ru>
2018-10-21 01:36:02 +0300

    feat(services/web/src/views/components/balance/Invoice.js): Добавил базовый компонент инвойса

---
+++ services/web/src/views/components/balance/Invoice.js
@@ -0,0 +1,23 @@
+import moment                   from 'moment';
+import React, { PureComponent } from 'react';
+import propTypes                from 'prop-types';
+
+class Invoice extends PureComponent {
+	static propTypes = {
+		createdAt: propTypes.string,
+		paidAt   : propTypes.string,
+		amount   : propTypes.number,
+	};
+	
+	render() {
+		return (
+			<div>
+				Создан: {moment(this.props.createdAt).format('MMMM Do YYYY, HH:mm:ss')}<br/>
+				Завершен: {moment(this.props.paidAt).format('MMMM Do YYYY, HH:mm:ss')}<br/>
+				Сердца: {this.props.amount}<br/>
+			</div>
+		);
+	}
+}
+
+export default Invoice;
