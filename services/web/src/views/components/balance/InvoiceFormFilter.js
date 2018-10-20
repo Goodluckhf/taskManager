a404891c8a0f4d61e7215c78f2683a7035f4bb9b
Konstantin <goodluckhf@yandex.ru>
2018-10-21 01:38:39 +0300

    feat(services/web/src/views/components/balance/InvoiceList.js): Компоненты списка с фильтрами

---
+++ services/web/src/views/components/balance/InvoiceFormFilter.js
@@ -0,0 +1,84 @@
+import React, { PureComponent, Fragment } from 'react';
+import propTypes from 'prop-types';
+import { Col, CustomInput } from 'reactstrap';
+
+class InvoiceFormFilter extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			status : this.props.status,
+			_status: this.props.status,
+		};
+		
+		this.filterAllId   = Math.random();
+		this.filterTopupId = Math.random();
+		this.filterTasksId = Math.random();
+	}
+	
+	static getDerivedStateFromProps(props, state) {
+		const newState = {};
+		if (props.status !== state._status) {
+			newState.status  = props.status;
+			newState._status = props.status;
+		}
+		
+		if (!Object.keys(newState).length) {
+			return null;
+		}
+		
+		return newState;
+	}
+	
+	static propTypes = {
+		change: propTypes.func,
+		status: propTypes.string,
+	};
+	
+	onFilterChange = (e) => {
+		this.setState({ status: e.target.value }, () => {
+			this.props.change(this.state.status);
+		});
+	};
+	
+	render() {
+		return (
+			<Fragment>
+				<Col xs='2'>
+					<CustomInput
+						id={this.filterAllId}
+						onChange={this.onFilterChange}
+						type='radio'
+						label='Все'
+						value='all'
+						checked={this.state.status === 'all'}
+						name='invoice_form_filter'
+					/>
+				</Col>
+				<Col xs='2'>
+					<CustomInput
+						id={this.filterTopupId}
+						onChange={this.onFilterChange}
+						type='radio'
+						label='Пополнение'
+						value='topup'
+						checked={this.state.status === 'topup'}
+						name='invoice_form_filter'
+					/>
+				</Col>
+				<Col xs='2'>
+					<CustomInput
+						id={this.filterTasksId}
+						onChange={this.onFilterChange}
+						type='radio'
+						label='Задачи'
+						value='tasks'
+						checked={this.state.status === 'tasks'}
+						name='invoice_form_filter'
+					/>
+				</Col>
+			</Fragment>
+		);
+	}
+}
+
+export default InvoiceFormFilter;
---
+++ services/web/src/views/components/balance/InvoiceList.js
@@ -0,0 +1,81 @@
+import Immutable                from 'immutable';
+import React, { PureComponent, Fragment } from 'react';
+import propTypes           from 'prop-types';
+import {
+	Card, CardHeader, CardBody,
+	Row, Col,
+}                          from 'reactstrap';
+import InvoiceFormFilter   from './InvoiceFormFilter';
+import Invoice             from './Invoice';
+import LikesTaskInvoice    from './LikesTaskInvoice';
+import RepostsTaskInvoice  from './RepostsTaskInvoice';
+import CommentsTaskInvoice from './CommentsTaskInvoice';
+import TopUpInvoice        from './TopUpInvoice';
+
+const invoiceMapper = {
+	LikesCommon   : LikesTaskInvoice,
+	RepostsCommon : RepostsTaskInvoice,
+	CommentsCommon: CommentsTaskInvoice,
+};
+
+class InvoiceList extends PureComponent {
+	static propTypes = {
+		error       : propTypes.object,
+		loading     : propTypes.bool,
+		filterChange: propTypes.func,
+		items       : propTypes.instanceOf(Immutable.List).isRequired,
+		filter      : propTypes.string.isRequired,
+	};
+	
+	render() {
+		const list = this.props.items.map((item) => {
+			const baseInvoice = <Invoice
+				amount={item.get('amount')}
+				createdAt={item.get('createAt')}
+				paidAt={item.get('paidAt')}
+			/>;
+			let Item = null;
+			if (!item.get('task')) {
+				Item = <TopUpInvoice
+					invoice={baseInvoice}
+					money={item.get('money')}
+					purse={item.get('purse')}
+				/>;
+			}
+			
+			const ConcreteInvoice = invoiceMapper[item.get('__t')];
+			if (!Item && !ConcreteInvoice) {
+				console.error('no concrete invoice', item.get('__t'), item.get('_id'));
+				Item = baseInvoice;
+			}
+			
+			return <Fragment key={item.get('_id')}>
+				<Col>
+					{Item || <ConcreteInvoice  invoice={baseInvoice} {...item.get('task').toJS()}/>}
+					<hr/>
+				</Col>
+			</Fragment>;
+		});
+		
+		return (
+			<Card>
+				<CardHeader>
+					<Row>
+						<Col><b>История платежей </b><span>{this.props.loading ? 'Загружаю...' : ''}</span></Col>
+						<InvoiceFormFilter
+							status={this.props.filter}
+							change={this.props.filterChange}
+						/>
+					</Row>
+				</CardHeader>
+				<CardBody>
+					<Row>
+						{list}
+					</Row>
+				</CardBody>
+			</Card>
+		);
+	}
+}
+
+export default InvoiceList;
