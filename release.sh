f43d703d423a058978a475340d0a572936b80a2b
Konstantin <goodluckhf@yandex.ru>
2018-10-28 18:27:19 +0300

    HOTFIX: проебал pull

--- release.sh
+++ release.sh
@@ -8,4 +8,5 @@ scp docker-compose.infra.yml just1ce@185.249.255.224:~/vk-tasks
 scp reload.sh just1ce@185.249.255.224:~/vk-tasks
 ssh just1ce@185.249.255.224 "cd ~/vk-tasks &&
 cat ./docker.pas | docker login --username just1ce --password-stdin &&
+RELEASE_VERSION=$1 docker-compose -f docker-compose.yml pull &&
 ./reload.sh $1"
