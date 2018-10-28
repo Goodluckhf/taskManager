660af0dbedfb8d3e1b558d7a7fd85edbe04eed60
Konstantin <goodluckhf@yandex.ru>
2018-10-28 12:58:17 +0300

    chore(reload.sh): Добваил скрипт для gracefull reload

---
+++ reload.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+docker-compose -f docker-compose.yml stop cron
+COMPOSE_HTTP_TIMEOUT=300 docker-compose -f docker-compose.yml stop api
+COMPOSE_HTTP_TIMEOUT=300 docker-compose -f docker-compose.yml stop task-consumer
+RELEASE_VERSION=$1 docker-compose -f docker-compose.yml up -d
+sleep 2
+RELEASE_VERSION=$1 docker-compose -f docker-compose.yml restart nginx
+sleep 4
+RELEASE_VERSION=$1 docker-compose -f docker-compose.yml restart cron
