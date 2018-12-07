a76d4f74e19a0668f326e986e662d10cfab025de
Konstantin Bubyakin <goodluckhf@yandex.ru>
2018-12-08 01:04:52 +0300

    refactor(services/api/tasks/LikesCheckTask.js): Переделал на postByLink

--- services/api/tasks/LikesCheckTask.js
+++ services/api/tasks/LikesCheckTask.js
@@ -1,6 +1,6 @@
 import mongoose from '../../../lib/mongoose';
 import BaseTask from './BaseTask';
-import LikeCheckRequest from '../api/amqpRequests/LikeCheckRequest';
+import PostByLinkRequest from '../api/amqpRequests/PostByLinkRequest';
 import LikesCommonTask from './LikesCommonTask';
 import TaskErrorFactory from '../api/errors/tasks/TaskErrorFactory';
 import BillingAccount from '../billing/BillingAccount';
@@ -13,21 +13,39 @@ class LikesCheckTask extends BaseTask {
 		const Task = mongoose.model('Task');
 		const serviceOrder = this.config.get('likesTask.serviceOrder');
 
-		const request = new LikeCheckRequest(this.config, {
+		const request = new PostByLinkRequest(this.config, {
 			postLink: this.taskDocument.postLink,
 			likesCount: this.taskDocument.likesCount,
 		});
 
+		let response = null;
 		try {
-			await this.rpcClient.call(request);
+			response = await this.rpcClient.call(request);
+		} catch (error) {
+			this.logger.error({
+				mark: 'likes',
+				postLink: this.taskDocument.postLink,
+				likesCount: this.taskDocument.likesCount,
+				service: serviceOrder[this.taskDocument.serviceIndex],
+				userId: this.taskDocument.user.id,
+				taskId: this.taskDocument.id,
+				error,
+			});
+			this.taskDocument.parentTask.status = Task.status.waiting;
+			await this.taskDocument.parentTask.save();
+			return;
+		}
+
+		if (response.likes >= this.taskDocument.likesCount) {
 			this.logger.info({
 				mark: 'likes',
 				message: 'Успешно накрутились',
-				userId: this.taskDocument.user.id,
-				taskId: this.taskDocument.id,
 				postLink: this.taskDocument.parentTask.postLink,
 				likesCount: this.taskDocument.parentTask.likesCount,
+				userId: this.taskDocument.user.id,
+				taskId: this.taskDocument.parentTask.id,
 			});
+
 			// Успешное выполнение
 			// Снимаем баллы с баланса
 			if (this.account instanceof BillingAccount) {
@@ -36,63 +54,70 @@ class LikesCheckTask extends BaseTask {
 
 			this.taskDocument.parentTask.status = Task.status.finished;
 			this.taskDocument.parentTask.lastHandleAt = new Date();
-		} catch (error) {
-			this.logger.error({
-				postLink: this.taskDocument.postLink,
-				likesCount: this.taskDocument.likesCount,
-				userId: this.taskDocument.user.id,
-				taskId: this.taskDocument.id,
-				error,
-			});
-
-			if (serviceOrder.length === this.taskDocument.serviceIndex + 1) {
-				if (this.account instanceof BillingAccount) {
-					await this.account.rollBack(this.taskDocument.parentTask);
-				}
-
-				const wrappedError = TaskErrorFactory.createError(
-					'likes',
-					error,
-					this.taskDocument.postLink,
-					this.taskDocument.parentTask.likesCount,
-				);
-
-				this.taskDocument.parentTask.lastHandleAt = new Date();
-				this.taskDocument.parentTask.status = Task.status.finished;
-				this.taskDocument.parentTask._error = wrappedError.toObject();
-				throw wrappedError;
-			}
-
-			this.taskDocument.parentTask.status = Task.status.pending;
-			await this.taskDocument.parentTask.save();
-			await this.taskDocument.parentTask.populate('user').execPopulate();
-
-			this.logger.info({
-				mark: 'likes',
-				message: 'Запускаем задачу на следущий сервис',
-				likesCount: this.taskDocument.likesCount,
-				service: serviceOrder[this.taskDocument.serviceIndex + 1],
-				userId: this.taskDocument.user.id,
-				taskId: this.taskDocument.id,
-			});
-
-			const likesTask = new LikesCommonTask({
-				billing: this.billing,
-				account: this.account,
-				serviceIndex: this.taskDocument.serviceIndex + 1,
-				logger: this.logger,
-				taskDocument: this.taskDocument.parentTask,
-				rpcClient: this.rpcClient,
-				config: this.config,
-				uMetrics: this.uMetrics,
-			});
-
-			await likesTask.handle();
-		} finally {
 			this.taskDocument.lastHandleAt = new Date();
 			this.taskDocument.status = Task.status.finished;
 			await Promise.all([this.taskDocument.save(), this.taskDocument.parentTask.save()]);
+			return;
 		}
+
+		this.logger.warn({
+			mark: 'likes',
+			postLink: this.taskDocument.postLink,
+			likesCount: this.taskDocument.likesCount,
+			userId: this.taskDocument.user.id,
+			taskId: this.taskDocument.id,
+			message: 'лайки не накрутились',
+		});
+
+		if (serviceOrder.length === this.taskDocument.serviceIndex + 1) {
+			if (this.account instanceof BillingAccount) {
+				await this.account.rollBack(this.taskDocument.parentTask);
+			}
+
+			const wrappedError = TaskErrorFactory.createError(
+				'likes',
+				new Error('лайки не накрутились'),
+				this.taskDocument.postLink,
+				this.taskDocument.parentTask.likesCount,
+			);
+
+			this.taskDocument.parentTask.lastHandleAt = new Date();
+			this.taskDocument.parentTask.status = Task.status.finished;
+			this.taskDocument.parentTask._error = wrappedError.toObject();
+			this.taskDocument.lastHandleAt = new Date();
+			this.taskDocument.status = Task.status.finished;
+			await Promise.all([this.taskDocument.save(), this.taskDocument.parentTask.save()]);
+			throw wrappedError;
+		}
+
+		this.taskDocument.parentTask.status = Task.status.pending;
+		await this.taskDocument.parentTask.save();
+		await this.taskDocument.parentTask.populate('user').execPopulate();
+
+		this.logger.info({
+			mark: 'likes',
+			message: 'Запускаем задачу на следущий сервис',
+			likesCount: this.taskDocument.likesCount,
+			service: serviceOrder[this.taskDocument.serviceIndex + 1],
+			userId: this.taskDocument.user.id,
+			taskId: this.taskDocument.id,
+		});
+
+		const likesTask = new LikesCommonTask({
+			billing: this.billing,
+			account: this.account,
+			serviceIndex: this.taskDocument.serviceIndex + 1,
+			logger: this.logger,
+			taskDocument: this.taskDocument.parentTask,
+			rpcClient: this.rpcClient,
+			config: this.config,
+			uMetrics: this.uMetrics,
+		});
+
+		await likesTask.handle();
+		this.taskDocument.lastHandleAt = new Date();
+		this.taskDocument.status = Task.status.finished;
+		await Promise.all([this.taskDocument.save(), this.taskDocument.parentTask.save()]);
 	}
 }
 
