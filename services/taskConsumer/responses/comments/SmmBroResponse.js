606a5c183101ca4cba81ebd3ad4a5480051e0fa1
Konstantin Bubyakin <goodluckhf@yandex.ru>
2018-12-11 19:53:27 +0300

    feat(services/taskConsumer/responses/comments/SmmBroResponse.js): Добавил Response для накрутки комментов для сервиса smmBro

---
+++ services/taskConsumer/responses/comments/SmmBroResponse.js
@@ -0,0 +1,53 @@
+import puppeteer from 'puppeteer';
+import Response from '../../../../lib/amqp/Response';
+import loginAction from '../../actions/smmBro/login';
+import createTask from '../../actions/smmBro/createTask';
+
+class SmmBroResponse extends Response {
+	/**
+	 * @return {String}
+	 */
+	// eslint-disable-next-line class-methods-use-this,
+	get method() {
+		return 'setComments_smmBro';
+	}
+
+	async process({ postLink, commentsCount, serviceCredentials: { login, password } }) {
+		const browser = await puppeteer.launch({
+			args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+			handleSIGINT: false,
+			headless: process.env.NODE_ENV === 'production',
+		});
+
+		try {
+			const page = await browser.newPage();
+			await loginAction(page, { login, password });
+
+			const errors = await createTask(page, {
+				postLink,
+				count: commentsCount,
+				type: 'comments',
+			});
+
+			if (errors.length) {
+				const error = new Error('Ошибки валидации');
+				error.messages = errors;
+				error.statusCode = 1;
+				throw error;
+			}
+
+			this.logger.info({
+				mark: 'comments',
+				service: 'smmBro',
+				message: 'Задача выполнилась',
+				postLink,
+				commentsCount,
+				login,
+			});
+		} finally {
+			await browser.close();
+		}
+	}
+}
+
+export default SmmBroResponse;
