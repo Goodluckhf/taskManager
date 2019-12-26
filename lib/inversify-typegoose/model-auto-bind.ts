a88b6ea54ba52e060838e5b567292e5c4042cd76
Konstantin Bubyakin <goodluckhf@yandex.ru>
2019-12-26 15:41:41 +0300

    feat(inversify-typegoose): параметр в декоратор для дискриминаторов

--- lib/inversify-typegoose/model-auto-bind.ts
+++ lib/inversify-typegoose/model-auto-bind.ts
@@ -1,12 +1,24 @@
 import 'reflect-metadata';
 import { interfaces } from 'inversify';
-import { getModelForClass } from '@typegoose/typegoose';
-import { modelMetadataSymbol } from './model.decorator';
+import { getDiscriminatorModelForClass, getModelForClass } from '@typegoose/typegoose';
+import { AnyParamConstructor } from '@typegoose/typegoose/lib/types';
+import { ModelMetadata, modelMetadataSymbol } from './model.decorator';
 import { getModelToken } from './get-model-token';
 
 export const modelAutoBind = (container: interfaces.Container): void => {
-	const modelsMetadataClass: [] = Reflect.getMetadata(modelMetadataSymbol, Reflect);
-	modelsMetadataClass.forEach(ModelClass => {
-		container.bind(getModelToken(ModelClass)).to(getModelForClass(ModelClass));
+	const modelsMetadataClass: ModelMetadata<any>[] = Reflect.getMetadata(
+		modelMetadataSymbol,
+		Reflect,
+	);
+	modelsMetadataClass.forEach(modelMetadata => {
+		let modelClass = getModelForClass(modelMetadata.modelClass as AnyParamConstructor<any>);
+		if (modelMetadata.discriminatorBaseClass) {
+			modelClass = getDiscriminatorModelForClass(
+				modelClass,
+				modelMetadata.discriminatorBaseClass as AnyParamConstructor<any>,
+			);
+		}
+
+		container.bind(getModelToken(modelMetadata.modelClass)).to(modelClass);
 	});
 };
--- lib/inversify-typegoose/model.decorator.ts
+++ lib/inversify-typegoose/model.decorator.ts
@@ -2,14 +2,30 @@ import 'reflect-metadata';
 
 export const modelMetadataSymbol = Symbol('modelMetadataSymbol');
 
-export const model = (): ClassDecorator => {
-	return (target): void => {
+export type ModelDecoratorConfig<T> = {
+	discriminatorClass?: ClassType<T>;
+};
+
+export interface ClassType<T> extends Function {
+	new (...args: any[]): T;
+}
+
+export type ModelMetadata<T> = {
+	modelClass: ClassType<T>;
+	discriminatorBaseClass?: ClassType<T>;
+};
+
+export function model<T>(config: ModelDecoratorConfig<T> = {}) {
+	return (target: ClassType<any>): void => {
 		if (!Reflect.hasMetadata(modelMetadataSymbol, Reflect)) {
 			Reflect.defineMetadata(modelMetadataSymbol, [], Reflect);
 		}
 
-		const models: [Function] = Reflect.getMetadata(modelMetadataSymbol, Reflect);
-		models.push(target);
+		const models: ModelMetadata<any>[] = Reflect.getMetadata(modelMetadataSymbol, Reflect);
+		models.push({
+			modelClass: target as ClassType<any>,
+			discriminatorBaseClass: (config.discriminatorClass as ClassType<any>) || undefined,
+		});
 		Reflect.defineMetadata(modelMetadataSymbol, models, Reflect);
 	};
-};
+}
