2efb2c1f6e7cf9b780a1e872fcb2fb017dc3e786
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-02 19:08:04 +0300

    feat(auth): мидлвара для проверки авторизации

--- services/api/auth/auth.middleware.ts
+++ services/api/auth/auth.middleware.ts
@@ -1,46 +1,23 @@
-import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
-import { inject, injectable } from 'inversify';
-import { IMiddleware } from 'koa-router';
-import { PassportStatic } from 'passport';
-import { MiddlewareInterface } from '../../../lib/framework/middleware.interface';
-import mongoose from '../../../lib/mongoose';
-import { ConfigInterface } from '../../../config/config.interface';
+import { Request, Response, NextFunction } from 'express';
+import { injectable } from 'inversify';
+import { BaseMiddleware } from 'inversify-express-utils';
+import { AccessDeniedException } from './access-denied.exception';
+import { User } from '../users/user';
 
 @injectable()
-export class AuthMiddleware implements MiddlewareInterface {
-	constructor(
-		@inject('Passport') private readonly passport: PassportStatic,
-		@inject('Config') private readonly config: ConfigInterface,
-	) {
-		this.applyPassport();
-	}
+export class AuthMiddleware extends BaseMiddleware {
+	async handler(req: Request, res: Response, next: NextFunction) {
+		const { user: principal } = this.httpContext;
+		const user = principal.details as User;
+		if (!(await principal.isAuthenticated())) {
+			res.status(401).send();
+			return;
+		}
 
-	applyPassport() {
-		const jwtOptions = {
-			jwtFromRequest: ExtractJwt.fromExtractors([
-				ExtractJwt.fromBodyField('jwt'),
-				ExtractJwt.fromUrlQueryParameter('jwt'),
-			]),
-			secretOrKey: this.config.get('jwt.secret'),
-		};
+		if (!(await principal.isInRole('premium'))) {
+			throw new AccessDeniedException(user._id.toString(), req);
+		}
 
-		this.passport.use(
-			new JwtStrategy(jwtOptions, async (payload, done) => {
-				try {
-					const user = await mongoose.model('User').findById(payload.id);
-					if (user) {
-						done(null, user);
-					} else {
-						done(null, false);
-					}
-				} catch (error) {
-					done(error);
-				}
-			}),
-		);
-	}
-
-	getRawMiddleware(): IMiddleware {
-		return this.passport.authenticate('jwt', { session: false });
+		next();
 	}
 }
