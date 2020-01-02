d9fcf2a60f92d6a0a5e1ce3a6a6d02d5076787a2
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-02 19:13:09 +0300

    refactor(di.container): ф-я создания контейнера

--- services/api/di.container.ts
+++ services/api/di.container.ts
@@ -1,18 +1,32 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import config from 'config';
-import * as passport from 'koa-passport';
+import mongoose from 'mongoose';
 import { ConfigInterface } from '../../config/config.interface';
 import logger from '../../lib/logger';
 import { LoggerInterface } from '../../lib/logger.interface';
+import { modelAutoBind } from '../../lib/inversify-typegoose/model-auto-bind';
+import { TaskAbstractFactoryInterface } from './task/task-abstract-factory.interface';
+import { TaskAbstractFactory } from './task/task-abstract.factory';
+import { TaskHandlerInterface } from './task/task-handler.interface';
+import { CommentsByStrategyTaskHandler } from './comments-by-strategy/comments-by-strategy-task.handler';
 
-const container = new Container({
-	autoBindInjectable: true,
-	defaultScope: 'Singleton',
-});
+import './comments-by-strategy/comments-by-strategy.controller';
+import { AuthMiddleware } from './auth/auth.middleware';
 
-container.bind<ConfigInterface>('Config').toConstantValue(config);
-container.bind<LoggerInterface>('Logger').toConstantValue(logger);
-container.bind('Passport').toConstantValue(passport.initialize());
+export function createContainer() {
+	const container = new Container({
+		autoBindInjectable: true,
+		defaultScope: 'Singleton',
+	});
 
-export default container;
+	container.bind<ConfigInterface>('Config').toConstantValue(config);
+	container.bind<LoggerInterface>('Logger').toConstantValue(logger);
+	container.bind<mongoose.Mongoose>('Mongoose').toConstantValue(mongoose);
+	container.bind<TaskAbstractFactoryInterface>(TaskAbstractFactory).toSelf();
+	container.bind(AuthMiddleware).toSelf();
+	container.bind<TaskHandlerInterface>('TaskHandlerInterface').to(CommentsByStrategyTaskHandler);
+
+	modelAutoBind(container);
+	return container;
+}
