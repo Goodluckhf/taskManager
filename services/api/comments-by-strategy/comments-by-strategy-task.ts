1460315ce64f8be423acf1d57494fef8be63f6c6
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-02 19:25:52 +0300

    feat(task): класс схема для задачи комметов по стретегии

---
+++ services/api/comments-by-strategy/comments-by-strategy-task.ts
@@ -0,0 +1,20 @@
+import { arrayProp, prop } from '@typegoose/typegoose';
+import moment from 'moment';
+import { model } from '../../../lib/inversify-typegoose/model.decorator';
+import { CommonTask } from '../task/common-task';
+import { CommentStrategy } from './comment-strategy';
+import { CommentByStrategyTaskInterface } from './comment-by-strategy-task.interface';
+import { DelayableTaskInterface } from '../task/delayable-task.interface';
+
+@model({ discriminatorClass: CommonTask })
+export class CommentsByStrategyTask extends CommonTask
+	implements CommentByStrategyTaskInterface, DelayableTaskInterface {
+	@prop({ required: true })
+	postLink: string;
+
+	@arrayProp({ items: CommentStrategy })
+	commentsStrategy: CommentStrategy[];
+
+	@prop({ required: true })
+	startAt: Date | moment.Moment;
+}
