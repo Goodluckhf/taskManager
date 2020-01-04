1d017a6bfa966d339df6ae5dca8dd67a83c30550
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-05 00:39:46 +0300

    feat(amqp/rpc): абстрактный класс для обработчика RPC

---
+++ lib/amqp/abstract-rpc-handler.ts
@@ -0,0 +1,9 @@
+export abstract class AbstractRpcHandler {
+	protected abstract method: string;
+
+	getMethod(): string {
+		return this.method;
+	}
+
+	abstract async handle(args: object): Promise<object>;
+}
