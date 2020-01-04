215568477705189359e24c81c477d16e88f9ac13
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-04 18:08:44 +0300

    feat(task): добавил endpoint для выполнения актвиных задач по крону

--- services/api/task/task.controller.ts
+++ services/api/task/task.controller.ts
@@ -5,16 +5,26 @@ import {
 	interfaces,
 	httpDelete,
 	requestParam,
+	httpGet,
 } from 'inversify-express-utils';
 import { inject } from 'inversify';
 import { AuthMiddleware } from '../auth/auth.middleware';
 import { User } from '../users/user';
 import { TaskService } from './task.service';
 import { TaskServiceInterface } from './task-service.interface';
+import GracefulStop from '../../../lib/graceful-stop';
+import { LoggerInterface } from '../../../lib/logger.interface';
+import { ActiveTasksExecutor } from './active-tasks-executor';
 
 @controller('/api')
 export class TaskController extends BaseHttpController {
 	@inject(TaskService) private readonly taskService: TaskServiceInterface;
+
+	@inject(ActiveTasksExecutor) private readonly activeTaskExecutor: ActiveTasksExecutor;
+
+	@inject(GracefulStop) private readonly gracefulStop: GracefulStop;
+
+	@inject('Logger') private readonly logger: LoggerInterface;
 
 	@httpDelete('/task/:id', AuthMiddleware)
 	async createTask(
@@ -25,6 +35,46 @@ export class TaskController extends BaseHttpController {
 			{
 				success: true,
 				data: await this.taskService.deleteOwnedByUser(principalUser.details as User, id),
+			},
+			200,
+		);
+	}
+
+	@httpGet('/task/handleActive')
+	async handleActive() {
+		if (this.gracefulStop.isStopping) {
+			this.logger.warn({
+				message: 'method call during graceful stopping',
+				method: 'TaskApi.handleActive',
+			});
+
+			return this.json(
+				{
+					success: true,
+					data: null,
+				},
+				200,
+			);
+		}
+
+		this.gracefulStop.setProcessing('handleActiveTasks');
+		this.activeTaskExecutor
+			.handleActive()
+			.then(() => {
+				this.gracefulStop.setReady('handleActiveTasks');
+			})
+			.catch(error => {
+				this.logger.error({
+					message: 'fatal error during handle tasks',
+					error,
+				});
+				this.gracefulStop.setReady('handleActiveTasks');
+			});
+
+		return this.json(
+			{
+				success: true,
+				data: null,
 			},
 			200,
 		);
