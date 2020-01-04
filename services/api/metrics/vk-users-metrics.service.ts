07a588259d73a8638439de493a15f4a08549819e
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-04 19:13:53 +0300

    refactor(vk-users-metrics.service): добавил зависимостью обертку

--- services/api/metrics/vk-users-metrics.service.ts
+++ services/api/metrics/vk-users-metrics.service.ts
@@ -1,12 +1,19 @@
 import { inject, injectable } from 'inversify';
+import { UMetrics } from 'umetrics';
 import { VkUserService } from '../vk-users/vk-user.service';
+import { UmetricsWrapper } from './umetrics-wrapper';
 
 @injectable()
 export class VkUsersMetricsService {
+	private readonly uMetrics: UMetrics;
+
 	constructor(
-		@inject('UMetrics') private readonly uMetrics,
+		@inject(UmetricsWrapper) private readonly uMetricsWrapper: UmetricsWrapper,
 		@inject(VkUserService) private readonly vkUserService: VkUserService,
-	) {}
+	) {
+		this.uMetrics = this.uMetricsWrapper.getUMetrics();
+		this.uMetrics.register(this.uMetrics.Metrics.Gauge, 'activeVkAccounts');
+	}
 
 	async storeCurrentValue() {
 		const count = this.vkUserService.countActive();
