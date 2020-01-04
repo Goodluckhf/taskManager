9fae491e363a77f65df667a4c04d8998179a6abb
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-04 19:58:50 +0300

    test(task/metrics): добавил мок

---
+++ services/api/tests/ts/mocks/task-metrics-service.mock.ts
@@ -0,0 +1,8 @@
+import { Mocked } from '../types';
+import { TaskMetricsServiceInterface } from '../../../metrics/task-metrics-service.interface';
+
+export const taskMetricsServiceMock: Mocked<TaskMetricsServiceInterface> = {
+	addDuration: jest.fn(),
+	increaseError: jest.fn(),
+	increaseSuccess: jest.fn(),
+};
