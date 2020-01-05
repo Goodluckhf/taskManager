677c19a18a59315918ad2db8bf456d59fa2dc992
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-05 23:52:33 +0300

    fix(test):  добавил фейковый user

--- services/api/tests/ts/fixtures/task-factory.ts
+++ services/api/tests/ts/fixtures/task-factory.ts
@@ -5,6 +5,9 @@ import { statuses } from '../../../task/status.constant';
 
 export function createTask(opts: Partial<CommonTask> = {}): CommonTask {
 	const taskPlain: CommonTask = {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+		// @ts-ignore
+		user: opts.user || { _id: new Types.ObjectId() },
 		__t: 'type',
 		__v: 0,
 		_error: undefined,
