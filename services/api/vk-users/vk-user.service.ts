ebd8e50b988876e21953fff950b894253646654c
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-23 23:51:11 +0300

    feat(vk-users): добавил метод получения по логину

--- services/api/vk-users/vk-user.service.ts
+++ services/api/vk-users/vk-user.service.ts
@@ -77,6 +77,10 @@ export class VkUserService {
 		return users[random(0, users.length - 1)];
 	}
 
+	async getCredentialsByLogin(login: string): Promise<VkUserCredentialsInterface> {
+		return this.VkUsersModel.findOne({ login }).lean();
+	}
+
 	async setInactive(login: string, reason: any) {
 		const user = await this.VkUsersModel.findOne({ login });
 		user.isActive = false;
