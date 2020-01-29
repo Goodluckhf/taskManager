bee19384be39e6059cc64e241fefa19ccbd06d35
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-01-30 00:03:04 +0300

    feat(comments): реализовал рандомную транслитерацию

---
+++ services/api/comments/comments-translit-replacer.ts
@@ -0,0 +1,73 @@
+import { getRandom } from '../../../lib/helper';
+
+const alphabetMapper = {
+	a: 'а',
+	А: 'A',
+	В: 'B',
+	У: 'Y',
+	О: 'O',
+	Р: 'P',
+	С: 'C',
+	C: 'С',
+	M: 'М',
+	М: 'M',
+	Т: 'T',
+	T: 'Т',
+	E: 'Е',
+	Е: 'E',
+	H: 'Н',
+	Н: 'H',
+	K: 'К',
+	К: 'K',
+	а: 'a',
+	e: 'е',
+	е: 'e',
+	y: 'у',
+	у: 'y',
+	и: 'u',
+	o: 'о',
+	о: 'o',
+	p: 'р',
+	р: 'p',
+	k: 'к',
+	к: 'k',
+	х: 'x',
+	x: 'х',
+	с: 'c',
+	c: 'с',
+};
+
+export class CommentsTranslitReplacer {
+	private replaceText(text: string): string {
+		return [...text]
+			.map(char => {
+				const random = getRandom(0, 100);
+				if (random > 50) {
+					return char;
+				}
+
+				const transiltChar = alphabetMapper[char];
+				if (!transiltChar) {
+					return char;
+				}
+
+				return transiltChar;
+			})
+			.join('');
+	}
+
+	randomReplace(originalText: string): string {
+		const link = originalText.match(/(https?:\/\/)?[^\s]+\.[^\s]+/gi);
+		if (link && link.length === 1) {
+			const textParts = originalText.split(link[0]);
+			const replacedParts = textParts.map(part => this.replaceText(part));
+			return `${replacedParts[0]}${link}${replacedParts[1]}`;
+		}
+
+		if (link && link.length > 1) {
+			return originalText;
+		}
+
+		return this.replaceText(originalText);
+	}
+}
---
+++ services/api/comments/commets-transit-replacer.spec.ts
@@ -0,0 +1,74 @@
+import { CommentsTranslitReplacer } from './comments-translit-replacer';
+
+describe('Транслитерация', () => {
+	let ctx: {
+		commentsTranslitReplacer: CommentsTranslitReplacer;
+	};
+
+	beforeEach(() => {
+		ctx = { commentsTranslitReplacer: new CommentsTranslitReplacer() };
+	});
+
+	it('Ссылка вконце', () => {
+		const link = 'http://vk.cc/asda';
+		const translited = ctx.commentsTranslitReplacer.randomReplace(
+			`Крутая шутка заказывала тут Просто супер ${link}`,
+		);
+		expect(translited).not.toBe(`Крутая шутка заказывала тут Просто супер ${link}`);
+		expect(translited).toMatch(link);
+	});
+
+	it('Ссылка вначале', () => {
+		const link = 'http://vk.cc/asda';
+		const translited = ctx.commentsTranslitReplacer.randomReplace(
+			`${link} Крутая шутка заказывала тут Просто супер`,
+		);
+		expect(translited).not.toBe(`${link} Крутая шутка заказывала тут Просто супер`);
+		expect(translited).toMatch(link);
+	});
+
+	it('Ссылка в центре', () => {
+		const link = 'http://vk.cc/asda';
+		const translited = ctx.commentsTranslitReplacer.randomReplace(
+			`Крутая шутка заказывала тут ${link} Просто супер`,
+		);
+		expect(translited).not.toBe(`Крутая шутка заказывала тут ${link} Просто супер`);
+		expect(translited).toMatch(link);
+	});
+
+	it('GET параметры', () => {
+		const link = 'http://vk.cc/asda?a=123&v=2';
+		const translited = ctx.commentsTranslitReplacer.randomReplace(
+			`Крутая шутка заказывала тут ${link}`,
+		);
+		expect(translited).not.toBe(`Крутая шутка заказывала тут ${link}`);
+		expect(translited).toMatch(link);
+	});
+
+	it('Без протокола', () => {
+		const link = 'vk.cc/asda?a=123&v=2';
+		const translited = ctx.commentsTranslitReplacer.randomReplace(
+			`Крутая шутка заказывала тут ${link}`,
+		);
+		expect(translited).not.toBe(`Крутая шутка заказывала тут ${link}`);
+		expect(translited).toMatch(link);
+	});
+
+	it('С поддоменом', () => {
+		const link = 'test.vk.cc/asda?a=123&v=2';
+		const translited = ctx.commentsTranslitReplacer.randomReplace(
+			`Крутая шутка заказывала тут ${link}`,
+		);
+		expect(translited).not.toBe(`Крутая шутка заказывала тут ${link}`);
+		expect(translited).toMatch(link);
+	});
+
+	it('С поддоменом и протоколом', () => {
+		const link = 'htpp://test-askkkka.vk.cc/asda?a=123&v=2';
+		const translited = ctx.commentsTranslitReplacer.randomReplace(
+			`Крутая шутка заказывала тут ${link}`,
+		);
+		expect(translited).not.toBe(`Крутая шутка заказывала тут ${link}`);
+		expect(translited).toMatch(link);
+	});
+});
