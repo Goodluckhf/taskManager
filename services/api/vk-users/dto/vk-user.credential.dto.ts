bce94242ee6403270ef27f32b38c346a1bda6197
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-02-01 18:49:09 +0300

    fix(vk-user): нужен декоратор prop

--- services/api/vk-users/dto/vk-user.credential.dto.ts
+++ services/api/vk-users/dto/vk-user.credential.dto.ts
@@ -1,18 +1,22 @@
 import { IsDefined, IsString, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
+import { prop } from '@typegoose/typegoose';
 import { VkUserCredentialsInterface } from '../vk-user-credentials.interface';
 import { ProxyCredentialsDto } from './proxy-credentials.dto';
 import { ProxyInterface } from '../../proxies/proxy.interface';
 
 export class VkUserCredentialDto implements VkUserCredentialsInterface {
 	@IsString()
+	@prop()
 	login: string;
 
 	@IsString()
+	@prop()
 	password: string;
 
 	@Type(() => ProxyCredentialsDto)
 	@IsDefined()
 	@ValidateNested()
+	@prop()
 	proxy: ProxyInterface;
 }
