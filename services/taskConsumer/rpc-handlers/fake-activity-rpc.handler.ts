02d0a9bcdcdaca0b2af18e4cead2414fc8bbddad
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-02-06 00:51:27 +0300

    feat(task-consumer): добавил обработчик задач на фейковую активность

---
+++ services/taskConsumer/rpc-handlers/fake-activity-rpc.handler.ts
@@ -0,0 +1,73 @@
+import { inject, injectable } from 'inversify';
+import { Browser } from 'puppeteer';
+import { AbstractRpcHandler } from '../../../lib/amqp/abstract-rpc-handler';
+import { VkUserCredentialsInterface } from '../../api/vk-users/vk-user-credentials.interface';
+import { VkAuthorizer } from '../actions/vk/vk-authorizer';
+import { LoggerInterface } from '../../../lib/logger.interface';
+import { createBrowserPage } from '../actions/create-page';
+import { getRandom } from '../../../lib/helper';
+import { FeedBrowser } from '../actions/vk/feed-browser';
+import { MessageReader } from '../actions/vk/message-reader';
+import { GroupBrowser } from '../actions/vk/group-browser';
+
+@injectable()
+export class FakeActivityRpcHandler extends AbstractRpcHandler {
+	protected method = 'fake_activity';
+
+	@inject(VkAuthorizer) private readonly vkAuthorizer: VkAuthorizer;
+
+	@inject(FeedBrowser) private readonly feedBrowser: FeedBrowser;
+
+	@inject(GroupBrowser) private readonly groupBrowser: GroupBrowser;
+
+	@inject(MessageReader) private readonly messageReader: MessageReader;
+
+	@inject('Logger') private readonly logger: LoggerInterface;
+
+	async handle({
+		userCredentials,
+	}: {
+		userCredentials: VkUserCredentialsInterface;
+	}): Promise<object> {
+		this.logger.info({
+			message: 'Задача на фейковую активность',
+			credentials: userCredentials,
+		});
+
+		let browser: Browser = null;
+		const canRetry = true;
+		try {
+			const { page, browser: _browser } = await createBrowserPage(userCredentials.proxy);
+			browser = _browser;
+
+			await this.vkAuthorizer.authorize(page, {
+				login: userCredentials.login,
+				password: userCredentials.password,
+				proxy: userCredentials.proxy,
+				remixsid: userCredentials.remixsid,
+			});
+
+			const randomNumber = getRandom(0, 100);
+			if (randomNumber < 30) {
+				await this.feedBrowser.browse(page);
+				return {};
+			}
+
+			if (randomNumber < 60) {
+				await this.messageReader.readMessages(page);
+				return {};
+			}
+
+			await this.groupBrowser.browse(page);
+
+			return {};
+		} catch (error) {
+			error.canRetry = typeof error.canRetry !== 'undefined' ? error.canRetry : canRetry;
+			throw error;
+		} finally {
+			if (browser) {
+				await browser.close();
+			}
+		}
+	}
+}
