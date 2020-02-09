d400a4572bd6df5f42cbf805dc4cfd63023ea3fb
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-02-09 12:11:57 +0300

    feat(fake-activity): rpc запрос для листания списка групп

---
+++ services/api/fake-activity/group-browse-rpc.request.ts
@@ -0,0 +1,19 @@
+import { AbstractRpcRequest } from '../../../lib/amqp/abstract-rpc-request';
+import { VkUserCredentialsInterface } from '../vk-users/vk-user-credentials.interface';
+
+type GroupBrowseArgument = {
+	userCredentials: VkUserCredentialsInterface;
+	isPopular: boolean;
+	isCommon: boolean;
+	scrollCount: number;
+};
+
+export class GroupBrowseRpcRequest extends AbstractRpcRequest {
+	protected readonly method = 'browse_groups';
+
+	protected readonly retriesLimit = 2;
+
+	setArguments(args: GroupBrowseArgument) {
+		this.args = args;
+	}
+}
