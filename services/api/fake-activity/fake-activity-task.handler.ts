0f74803809a6eeb14c9723a3fbd8cfdb20e98180
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-02-09 12:53:36 +0300

    refactor(fake-activity): обработка и отправка запросов задач

--- services/api/fake-activity/fake-activity-task.handler.ts
+++ services/api/fake-activity/fake-activity-task.handler.ts
@@ -1,20 +1,21 @@
 import { inject, injectable } from 'inversify';
+import bluebird from 'bluebird';
 import { TaskHandlerInterface } from '../task/task-handler.interface';
 import { FakeActivityTask } from './fake-activity.task';
 import { VkUserService } from '../vk-users/vk-user.service';
-import { RpcRequestFactory } from '../../../lib/amqp/rpc-request.factory';
-import { FakeActivityRpcRequest } from './fake-activity-rpc.request';
 import RpcClient from '../../../lib/amqp/rpc-client';
 import { LoggerInterface } from '../../../lib/logger.interface';
 import { FakeActivityTaskService } from './fake-activity-task.service';
 import { User } from '../users/user';
 import { AuthExceptionCatcher } from '../vk-users/auth-exception.catcher';
+import { FakeActivityRandomizerFactory } from './fake-activity-randomizer.factory';
 
 @injectable()
 export class FakeActivityTaskHandler implements TaskHandlerInterface {
 	constructor(
 		@inject(VkUserService) private readonly vkUserService: VkUserService,
-		@inject(RpcRequestFactory) private readonly rpcRequestFactory: RpcRequestFactory,
+		@inject(FakeActivityRandomizerFactory)
+		private readonly fakeActivityRandomizerFactory: FakeActivityRandomizerFactory,
 		@inject(RpcClient) private readonly rpcClient: RpcClient,
 		@inject('Logger') private readonly logger: LoggerInterface,
 		@inject(FakeActivityTaskService)
@@ -24,23 +25,32 @@ export class FakeActivityTaskHandler implements TaskHandlerInterface {
 
 	async handle(task: FakeActivityTask) {
 		const vkUser = await this.vkUserService.getCredentialsByLogin(task.login);
-		const request = this.rpcRequestFactory.create(FakeActivityRpcRequest);
-		request.setArguments({ userCredentials: vkUser });
-		try {
-			await this.rpcClient.call(request);
-		} catch (error) {
-			const catched = await this.authExceptionCatcher.catch(error, vkUser);
-			if (catched) {
-				return;
-			}
+		const rpcRequests = this.fakeActivityRandomizerFactory.getRandomTasks({
+			userCredentials: vkUser,
+		});
 
-			this.logger.error({
-				message: 'Ошибка при фейковой активности',
-				taskId: task._id.toString(),
-				login: task.login,
-				error,
-			});
-		}
+		await bluebird.map(
+			rpcRequests,
+			async rpcRequest => {
+				try {
+					await this.rpcClient.call(rpcRequest);
+				} catch (error) {
+					const catched = await this.authExceptionCatcher.catch(error, vkUser);
+					if (catched) {
+						return;
+					}
+
+					this.logger.error({
+						message: 'Ошибка при фейковой активности',
+						taskId: task._id.toString(),
+						taskArguments: rpcRequest.getArguments(),
+						login: task.login,
+						error,
+					});
+				}
+			},
+			{ concurrency: 1 },
+		);
 
 		await this.fakeActivityTaskService.create(task.user as User, task.login);
 	}
