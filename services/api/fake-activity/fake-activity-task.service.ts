9ef7b70f2128a258b5953f516f3d4f563e586327
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-02-10 00:12:55 +0300

    feat(fake-activity): добавил интервал в конфиг

--- services/api/fake-activity/fake-activity-task.service.ts
+++ services/api/fake-activity/fake-activity-task.service.ts
@@ -1,23 +1,25 @@
 import { ModelType } from '@typegoose/typegoose/lib/types';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import moment from 'moment';
 import { injectModel } from '../../../lib/inversify-typegoose/inject-model';
 import { FakeActivityTask } from './fake-activity.task';
 import { User } from '../users/user';
 import { getRandom } from '../../../lib/helper';
 import { statuses } from '../task/status.constant';
+import { ConfigInterface } from '../../../config/config.interface';
 
 @injectable()
 export class FakeActivityTaskService {
 	constructor(
 		@injectModel(FakeActivityTask)
 		private readonly FakeActivityTaskModel: ModelType<FakeActivityTask>,
+		@inject('Config') private readonly config: ConfigInterface,
 	) {}
 
 	async create(user: User, login: string) {
 		const newTask = new this.FakeActivityTaskModel();
 		newTask.login = login;
-		const random = getRandom(0, 60 * 60 * 2);
+		const random = getRandom(0, this.config.get('fakeActivityTask.interval'));
 		newTask.startAt = moment().add(random, 's');
 		await newTask.save();
 	}
