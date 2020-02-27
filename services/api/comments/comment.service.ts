46ed59ca298bdd64e0c80a9424d137f568127cba
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-02-27 14:18:10 +0300

    refactor(comments): обновление сессии для простановки комментов

--- services/api/comments/comment.service.ts
+++ services/api/comments/comment.service.ts
@@ -6,6 +6,7 @@ import { PostCommentRpcRequest } from './post-comment-rpc.request';
 import { RpcRequestFactory } from '../../../lib/amqp/rpc-request.factory';
 import { PostCommentArgInterface } from './post-comment-arg.interface';
 import { PostCommentResponse } from './post-comment.response';
+import { VkUserService } from '../vk-users/vk-user.service';
 
 @injectable()
 export class CommentService {
@@ -14,13 +15,16 @@ export class CommentService {
 		@inject(RpcClient) private readonly rpcClient: RpcClient,
 		@inject('Logger') private readonly logger: LoggerInterface,
 		@inject(RpcRequestFactory) private readonly rpcRequestFactory: RpcRequestFactory,
+		@inject(VkUserService) private readonly vkUserService: VkUserService,
 	) {}
 
 	async postComment(args: PostCommentArgInterface): Promise<PostCommentResponse> {
 		const rpcRequest = this.rpcRequestFactory.create(PostCommentRpcRequest);
 		rpcRequest.setArguments(args);
 		try {
-			return await this.rpcClient.call<PostCommentResponse>(rpcRequest);
+			const response = await this.rpcClient.call<PostCommentResponse>(rpcRequest);
+			await this.vkUserService.updateSession(args.credentials.login, response.remixsid);
+			return response;
 		} catch (error) {
 			this.logger.error({
 				mark: 'setComments',
--- services/api/comments/post-comment.response.ts
+++ services/api/comments/post-comment.response.ts
@@ -1,3 +1,7 @@
-export class PostCommentResponse {
+import { SessionTokenRpcResponseInterface } from '../vk-users/session-token-rpc-response.interface';
+
+export class PostCommentResponse implements SessionTokenRpcResponseInterface {
 	commentId: string;
+
+	remixsid: string;
 }
