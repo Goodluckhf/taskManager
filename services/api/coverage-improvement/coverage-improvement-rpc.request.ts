f02de30ea5af0a287884b62dfe0c5e0abaacad46
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-03-08 13:03:49 +0300

    feat(coverage-improvement): убрал ретраи и добавил приоритет

--- services/api/coverage-improvement/coverage-improvement-rpc.request.ts
+++ services/api/coverage-improvement/coverage-improvement-rpc.request.ts
@@ -9,7 +9,9 @@ type CoverageImprovementArgument = {
 export class CoverageImprovementRpcRequest extends AbstractRpcRequest {
 	protected readonly method = 'coverage_improvement';
 
-	protected readonly retriesLimit = 1;
+	protected readonly retriesLimit = 0;
+
+	protected priority = 3;
 
 	setArguments(args: CoverageImprovementArgument) {
 		this.args = args;
