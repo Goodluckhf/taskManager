0e743e39231a67529af2eaaf3dce69a5cbb01b49
Konstantin Bubyakin <goodluckhf@yandex.ru>
2020-03-25 22:54:57 +0300

    feat(vk-users): добавил сбор метрик при бане

--- services/api/vk-users/auth-exception.catcher.ts
+++ services/api/vk-users/auth-exception.catcher.ts
@@ -2,11 +2,14 @@ import { inject, injectable } from 'inversify';
 import { LoggerInterface } from '../../../lib/logger.interface';
 import { VkUserService } from './vk-user.service';
 import { VkUserCredentialsInterface } from './vk-user-credentials.interface';
+import { VkUsersBanMetricsService } from '../metrics/vk-users-ban-metrics.service';
 
 @injectable()
 export class AuthExceptionCatcher {
 	constructor(
 		@inject(VkUserService) private readonly vkUserService: VkUserService,
+		@inject(VkUsersBanMetricsService)
+		private readonly vkUsersBanMetricsService: VkUsersBanMetricsService,
 		@inject('Logger') private readonly logger: LoggerInterface,
 	) {}
 
@@ -28,6 +31,7 @@ export class AuthExceptionCatcher {
 			});
 
 			await this.vkUserService.setInactive(vkUserCredentials.login, error);
+			await this.vkUsersBanMetricsService.increaseBannedBot(vkUserCredentials.login);
 			return true;
 		}
 
